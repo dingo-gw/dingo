2ec57c30ba02dfd3f2e637a3ceb4431cfff67dee
Maximilian Dax <mdax@localadmins-MBP.is.localnet>
2021-06-22 09:42:17 +0200

    Packaging for PyPI.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+VERSION = '0.0.1'
+
+setup(name='dingo',
+      description='A library for deep bayesian inference with GNPE.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/dingo-gw/dingo-devel',
+      license="MIT",
+      version=VERSION,
+      packages=['dingo', 'dingo.core', 'dingo.core', 'dingo.gw'],
+      package_dir={'dingo': 'dingo'},
+      python_requires='>=3.6',
+      install_requires=[
+          'numpy',
+          'nflows'
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"]
+      )
