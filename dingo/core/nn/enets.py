59f48d5f850afacd9c373dc84cd9cc9e5bb1bb0a
Maximilian Dax <mdax@localadmins-MBP.is.localnet>
2021-06-24 14:14:51 +0200

    Finished Code for linear projection module for embedding network.

--- dingo/core/nn/enets.py
+++ dingo/core/nn/enets.py
@@ -16,7 +16,7 @@ class LinearProjectionRB(nn.Module):
     def __init__(self,
                  input_dims,
                  n_rb,
-                 v_rb,
+                 V_rb,
                  ):
         """
         Parameters
@@ -27,13 +27,13 @@ class LinearProjectionRB(nn.Module):
         n_rb : int
             number of reduced basis elements used for projection
             the output dimension is 2 * n_rb * num_blocks
-        v_rb : list of np.arrays
+        V_rb : list of np.arrays
             list with v matrices of the reduced basis SVD projection
         """
 
         super(LinearProjectionRB, self).__init__()
 
-        self.test_dimensions(input_dims, n_rb, v_rb)
+        self.test_dimensions(input_dims, n_rb, V_rb)
         self.input_dims = input_dims
         self.num_blocks, self.num_channels, self.num_bins = self.input_dims
         self.n_rb = n_rb
@@ -48,7 +48,7 @@ class LinearProjectionRB(nn.Module):
         self.layers = nn.ModuleList(layers)
 
         # initialize layers with
-        self.init_layers(v_rb)
+        self.init_layers(V_rb)
 
         print('Module 1: {:} -> {:}'.format(self.input_dim, self.output_dim))
 
@@ -60,20 +60,20 @@ class LinearProjectionRB(nn.Module):
     def output_dim(self):
         return 2 * self.n_rb * self.num_blocks
 
-    def test_dimensions(self, input_dims, n_rb, v_rb):
+    def test_dimensions(self, input_dims, n_rb, V_rb):
         assert len(input_dims) == 3, 'Exactly 3 axes required: blocks, channels, bins'
         assert input_dims[1] >= 2, 'Number of channels needs to be at least 2, for real and imaginary parts.'
-        assert len(v_rb) == input_dims[0], 'There must be exactly one reduced basis matrix v for each block.'
-        for v in v_rb:
-            assert v.shape[0] == input_dims[2], 'Number of input bins and number of rows in v_rb need to match.'
+        assert len(V_rb) == input_dims[0], 'There must be exactly one reduced basis matrix v for each block.'
+        for v in V_rb:
+            assert v.shape[0] == input_dims[2], 'Number of input bins and number of rows in V_rb need to match.'
             assert v.shape[1] >= n_rb, 'More reduced basis elements requested than available.'
 
 
-    def init_layers(self, v_rb):
+    def init_layers(self, V_rb):
         # loop through layers and initialize them individually
         for ind,layer in enumerate(self.layers):
             print('Initializing reduced basis projection layer for block {:}.'.format(ind))
-            v = v_rb[ind]
+            v = V_rb[ind]
             # truncate v to n_rb basis elements
             v = v[:,:self.n_rb]
             v_real, v_imag = torch.from_numpy(v.real).float(), torch.from_numpy(v.imag).float()
@@ -97,65 +97,6 @@ class LinearProjectionRB(nn.Module):
 
 
 
+
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    from scipy import linalg
-
-    num_bins = 200
-    n = 1000
-    n_rb = 10
-    x = np.linspace(0,1,num_bins)
-    theta = np.random.rand(n, 2)
-    y1, y2 = np.zeros((n, num_bins)), np.zeros((n, num_bins))
-    for i in range(n):
-        y1[i,:] = np.cos(x*(0.1+theta[i,0])*20 + theta[i,1])
-        y2[i,:] = np.cos(x*(0.1+theta[i,0])*20 + theta[i,1]) * x
-    y1 = y1 * np.exp(2j*np.pi*x**2)
-    y2 = y2 * np.exp(2j*np.pi*x**2)
-
-    _, _, Vh1 = linalg.svd(y1)
-    V1 = Vh1.T.conj()
-    _, _, Vh2 = linalg.svd(y2)
-    V2 = Vh2.T.conj()
-
-    plt.plot(x, y1[0].real)
-    plt.plot(x, (y1[0]@V1[:,:n_rb]@Vh1[:n_rb,:]).real)
-    plt.plot(x, (y1[0]@V2[:,:n_rb]@Vh2[:n_rb,:]).real)
-    plt.show()
-
-    plt.plot(x, (y1[0]@V1[:,:n_rb]@Vh1[:n_rb,:]).real - y1[0].real)
-    plt.plot(x, (y1[0]@V2[:,:n_rb]@Vh2[:n_rb,:]).real - y1[0].real)
-    plt.show()
-
-    plt.plot(x, (y2[0]@V1[:,:n_rb]@Vh1[:n_rb,:]).real - y2[0].real)
-    plt.plot(x, (y2[0]@V2[:,:n_rb]@Vh2[:n_rb,:]).real - y2[0].real)
-    plt.show()
-
-    layer1 = LinearProjectionRB(input_dims=(2,3,num_bins), n_rb=10, v_rb=[V1, V2])
-
-    y_a = torch.zeros((n, 2, 3, num_bins))
-    y_a[:,0,0,:] = torch.from_numpy(y1.real).float()
-    y_a[:,0,1,:] = torch.from_numpy(y1.imag).float()
-    y_a[:,1,0,:] = torch.from_numpy(y2.real).float()
-    y_a[:,1,1,:] = torch.from_numpy(y2.imag).float()
-
-    y_tmp = np.ones_like(y1)
-    y_b = torch.zeros((n, 2, 3, num_bins))
-    y_b[:,0,0,:] = torch.from_numpy(y_tmp.real).float()
-    y_b[:,0,1,:] = torch.from_numpy(y_tmp.imag).float()
-    y_b[:,1,0,:] = torch.from_numpy(y_tmp.real).float()
-    y_b[:,1,1,:] = torch.from_numpy(y_tmp.imag).float()
-
-    out_a = np.array(layer1(y_a).detach())
-    out_b = np.array(layer1(torch.ones_like(y_a)).detach())
-
-    ref_a = np.concatenate(((y1[:]@V1[:,:n_rb]).real,
-                            (y1[:]@V1[:,:n_rb]).imag,
-                            (y2[:]@V1[:,:n_rb]).real,
-                            (y2[:]@V1[:,:n_rb]).imag), axis=1)
-    ref_b = np.concatenate(((y_tmp[:]@V1[:,:n_rb]).real,
-                            (y_tmp[:]@V1[:,:n_rb]).imag,
-                            (y_tmp[:]@V1[:,:n_rb]).real,
-                            (y_tmp[:]@V1[:,:n_rb]).imag), axis=1)
-
-    print('done')
+    pass
