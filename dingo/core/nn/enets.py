0e6cf50ff4b438d8d4dd69d52ad32f8733b20b70
Maximilian Dax <maximilian.dax@tuebingen.mpg.de>
2021-07-05 07:41:56 +0200

    Added dense residual network to enets.

--- dingo/core/nn/enets.py
+++ dingo/core/nn/enets.py
@@ -1,6 +1,6 @@
 """Implementation of embedding networks."""
 
-from typing import Tuple
+from typing import Tuple, Callable
 import torch
 import numpy as np
 import torch.nn as nn
@@ -38,7 +38,7 @@ class LinearProjectionRB(nn.Module):
     def __init__(self,
                  input_dims: Tuple[int, int, int],
                  n_rb: int,
-                 V_rb_list : Tuple,
+                 V_rb_list: Tuple,
                  ):
         """
         Parameters
@@ -140,63 +140,79 @@ class LinearProjectionRB(nn.Module):
         return x
 
 
-# class DenseResidualNet(nn.Module):
-#     """
-#     TODO
-#     """
-#
-#     def __init__(self,
-#                  input_dim: int,
-#                  output_dim: int,
-#                  hidden_dims: list,
-#                  activation=F.elu,
-#                  dropout=0.0,
-#                  batch_norm=True,
-#                  ):
-#         """
-#         Parameters
-#         ----------
-#         input_dim : int
-#             dimension of the input to this module
-#         output_dim : int
-#             output dimension of this module
-#         hidden_dims : list
-#             list with dimensions of hidden layers of this module
-#         """
-#
-#         super(DenseResidualNet, self).__init__()
-#         self.input_dim = input_dim
-#         self.output_dim = output_dim
-#         self.hidden_dims = hidden_dims
-#         self.num_res_blocks = len(self.hidden_dims)
-#
-#         self.initial_layer = nn.Linear(self.input_dim, hidden_dims[0])
-#         self.blocks = nn.ModuleList(
-#             [
-#                 ResidualBlock(
-#                     features=self.hidden_dims[n],
-#                     context_features=None,
-#                     activation=activation,
-#                     dropout_probability=dropout,
-#                     use_batch_norm=batch_norm,
-#                 )
-#                 for n in range(self.num_res_blocks)
-#             ]
-#         )
-#         self.resize_layers = nn.ModuleList(
-#             [
-#                 nn.Linear(self.hidden_dims[n - 1], self.hidden_dims[n])
-#                 if self.hidden_dims[n - 1] != self.hidden_dims[n]
-#                 else nn.Identity()
-#                 for n in range(1, self.num_res_blocks)
-#             ]
-#         )
-#         self.final_layer = nn.Linear(self.hidden_dims[-1], self.output_dim)
-#
-#         print('Module 1: {:} -> {:}'.format(
-#             self.input_dim * self.input_channels * self.input_blocks,
-#             self.output_dim))
-#         print('Module 2: {:} -> {:}'.format(in_features, out_features))
+class DenseResidualNet(nn.Module):
+    """
+    A nn.Module consisting of a sequence of dense residual blocks. This is
+    used to embed high dimensional input to a compressed output. Linear
+    resizing layers are used for resizing the input and output to match the
+    first and last hidden dimension, respectively.
+
+    Layer specs
+    --------
+        input dimension:    (batch_size, input_dim)
+        output dimension:   (batch_size, output_dim)
+    """
+
+    def __init__(self,
+                 input_dim: int,
+                 output_dim: int,
+                 hidden_dims: Tuple,
+                 activation: Callable = F.elu,
+                 dropout: int = 0.0,
+                 batch_norm: bool = True,
+                 ):
+        """
+        Parameters
+        ----------
+        input_dim : int
+            dimension of the input to this module
+        output_dim : int
+            output dimension of this module
+        hidden_dims : tuple
+            tuple with dimensions of hidden layers of this module
+        activation: callable
+            activation function used in residual blocks
+        dropout: int
+            dropout probability for residual blocks used for reqularization
+        batch_norm: bool
+            flag that specifies whether to use batch normalization
+        """
+
+        super(DenseResidualNet, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.hidden_dims = hidden_dims
+        self.num_res_blocks = len(self.hidden_dims)
+
+        self.initial_layer = nn.Linear(self.input_dim, hidden_dims[0])
+        self.blocks = nn.ModuleList(
+            [
+                ResidualBlock(
+                    features=self.hidden_dims[n],
+                    context_features=None,
+                    activation=activation,
+                    dropout_probability=dropout,
+                    use_batch_norm=batch_norm,
+                )
+                for n in range(self.num_res_blocks)
+            ]
+        )
+        self.resize_layers = nn.ModuleList(
+            [
+                nn.Linear(self.hidden_dims[n - 1], self.hidden_dims[n])
+                if self.hidden_dims[n - 1] != self.hidden_dims[n]
+                else nn.Identity()
+                for n in range(1, self.num_res_blocks)
+            ]
+            + [nn.Linear(self.hidden_dims[-1], self.output_dim)]
+        )
+
+    def forward(self, x):
+        x = self.initial_layer(x)
+        for block, resize_layer in zip(self.blocks, self.resize_layers):
+            x = block(x, context=None)
+            x = resize_layer(x)
+        return x
 
 
 if __name__ == '__main__':
