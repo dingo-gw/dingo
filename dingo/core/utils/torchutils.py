4c8949e14b7d9b59a5b4c1eac9da4cea2f165aa6
Maximilian Dax <maximilian.dax@tuebingen.mpg.de>
2021-07-07 11:51:18 +0200

    Added function to count model parameters to utils.

--- dingo/core/utils/torchutils.py
+++ dingo/core/utils/torchutils.py
@@ -43,3 +43,28 @@ def forward_pass_with_unpacked_tuple(
         return model(*x)
     else:
         return model(x)
+
+
+def get_number_of_model_parameters(
+        model: nn.Module,
+        requires_grad_flags: tuple = (True, False),
+):
+    """
+    Counts parameters of the module. The list requires_grad_flag can be used
+    to specify whether all parameters should be counted, or only those with
+    requires_grad = True or False.
+    :param model: nn.Module
+        model
+    :param requires_grad_flags: tuple
+        tuple of bools, for requested requires_grad flags
+    :return:
+        number of parameters of the model with requested required_grad flags
+    """
+    num_params = 0
+    for p in list(model.parameters()):
+        if p.requires_grad in requires_grad_flags:
+            n = 1
+            for s in list(p.size()):
+                n = n * s
+            num_params += n
+    return num_params
