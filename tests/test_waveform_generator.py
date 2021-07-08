8d2393ff3d10a9994f1cddd879be4dacabc658d9
Michael Pürrer <Michael.Puerrer@aei.mpg.de>
2021-07-08 17:01:05 +0200

    Start coding StandardizeParameters and StandardizedDistribution classes

--- tests/test_waveform_generator.py
+++ tests/test_waveform_generator.py
@@ -1,7 +1,10 @@
 from dingo.gw.domains import UniformFrequencyDomain, TimeDomain
-from dingo.gw.waveform_generator import WaveformGenerator
+from dingo.gw.waveform_generator import WaveformGenerator, StandardizedDistribution
 import pytest
 import numpy as np
+import torch
+import torch.distributions
+
 
 @pytest.fixture
 def uniform_fd_domain():
@@ -24,3 +27,17 @@ def test_waveform_generator_FD(uniform_fd_domain, aligned_spin_wf_parameters):
     hp, hc = wf_gen.generate_hplus_hcross(parameters)
     assert len(hp) == len(domain)
     assert domain()[domain.frequency_mask][0] == domain.f_min
+
+
+def test_standardized_distribution():
+    """Check standardization of samples from a multi-normal distribution."""
+    mean_ = torch.tensor([3.0, 2.0, 8.0])
+    std_ = torch.tensor([2.0, 4.0, 7.0])
+    base_dist = torch.distributions.Normal(mean_, std_)
+
+    std_dist = StandardizedDistribution(base_dist, mean_, std_)
+    Y2 = std_dist.sample((100000,))
+
+    tol = 0.01
+    assert np.all(np.abs(torch.mean(Y2, dim=0).numpy()) < tol)
+    assert np.all(np.abs(torch.std(Y2, dim=0).numpy()) - np.ones(3) < tol)
