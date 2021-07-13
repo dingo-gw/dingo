0056289602dcfdef7d8642895e2cd349ebc6a406
Maximilian Dax <maximilian.dax@tuebingen.mpg.de>
2021-07-13 18:00:18 +0200

    Added functionality for PosteriorModel to initialize, save and load optimizers and lr schedulers.

--- dingo/core/models/posterior_model.py
+++ dingo/core/models/posterior_model.py
@@ -4,6 +4,7 @@ TODO: Docstring
 
 from typing import Callable
 import torch
+import dingo.core.utils.torchutils as torchutils
 
 
 class PosteriorModel:
@@ -41,6 +42,10 @@ class PosteriorModel:
                  model_builder: Callable,
                  model_kwargs: dict = None,
                  model_filename: str = None,
+                 optimizer_kwargs: dict = None,
+                 scheduler_kwargs: dict = None,
+                 epoch: int = 1,
+                 init_for_training: bool = False,
                  ):
         """
         TODO: Docstring
@@ -48,11 +53,21 @@ class PosteriorModel:
         self.model_builder = model_builder
         self.model_kwargs = model_kwargs
 
+        self.epoch = epoch
+        self.optimizer_kwargs = optimizer_kwargs
+        self.optimizer = None
+        self.scheduler_kwargs = scheduler_kwargs
+        self.scheduler = None
+
         # build model
         if model_filename is not None:
-            self.load_model(model_filename)
+            self.load_model(model_filename,
+                            load_training_info=init_for_training)
         else:
             self.initialize_model()
+            # initialize for training
+            if init_for_training:
+                self.initialize_optimizer_and_scheduler()
 
         # TODO: initialize training and  data loader
 
@@ -64,16 +79,21 @@ class PosteriorModel:
         """
         self.model = self.model_builder(**self.model_kwargs)
 
-    def initialize_training(self,
-                            epoch: int = 0,
-                            optimizer=None,
-                            scheduler=None,
-                            ):
-        self.epoch = epoch
-        # TODO
+    def initialize_optimizer_and_scheduler(self):
+        """
+        Initializes the optimizer and scheduler with self.optimizer_kwargs
+        and self.scheduler_kwargs, respectively.
+        """
+        if self.optimizer_kwargs is not None:
+            self.optimizer = torchutils.get_optimizer_from_kwargs(
+                self.model.parameters(), **self.optimizer_kwargs)
+        if self.scheduler_kwargs is not None:
+            self.scheduler = torchutils.get_scheduler_from_kwargs(
+                self.optimizer, **self.scheduler_kwargs)
 
     def save_model(self,
                    model_filename: str,
+                   save_training_info: bool = True,
                    ):
         """
         Save the posterior model to the disk.
@@ -82,7 +102,7 @@ class PosteriorModel:
         ----------
         model_filename: str
             filename for saving the model
-        save_training_info: bool #TODO: save information for training
+        save_training_info: bool
             specifies whether information required to proceed with training is
             saved, e.g. optimizer state dict
 
@@ -90,15 +110,23 @@ class PosteriorModel:
         model_dict = {
             'model_kwargs': self.model_kwargs,
             'model_state_dict': self.model.state_dict(),
-            # 'training_info': None,
+            'epoch': self.epoch,
             # 'data_conditioning': None,
         }
+        if save_training_info:
+            model_dict['optimizer_kwargs'] = self.optimizer_kwargs
+            model_dict['scheduler_kwargs'] = self.scheduler_kwargs
+            if self.optimizer is not None:
+                model_dict['optimizer_state_dict'] = self.optimizer.state_dict()
+            if self.scheduler is not None:
+                model_dict['scheduler_state_dict'] = self.scheduler.state_dict()
+            # TODO
+
         torch.save(model_dict, model_filename)
-
-
 
     def load_model(self,
                    model_filename: str,
+                   load_training_info: bool = True,
                    ):
         """
         Load a posterior model from the disk.
@@ -107,21 +135,31 @@ class PosteriorModel:
         ----------
         model_filename: str
             path to saved model
-        save_training_info: bool #TODO: load information for training
+        load_training_info: bool #TODO: load information for training
             specifies whether information required to proceed with training is
             loaded, e.g. optimizer state dict
         """
-        # with torch.load(model_filename) as d:
-        #     self.model_kwargs = d['model_kwargs']
-        #     self.initialize_model()
-        #     self.model.load_state_dict(d['model_state_dict'])
 
         d = torch.load(model_filename)
+
         self.model_kwargs = d['model_kwargs']
         self.initialize_model()
         self.model.load_state_dict(d['model_state_dict'])
 
+        self.epoch = d['epoch']
 
+        if load_training_info:
+            if 'optimizer_kwargs' in d:
+                self.optimizer_kwargs = d['optimizer_kwargs']
+            if 'scheduler_kwargs' in d:
+                self.scheduler_kwargs = d['scheduler_kwargs']
+            # initialize optimizer and scheduler
+            self.initialize_optimizer_and_scheduler()
+            # load optimizer and scheduler state dict
+            if 'optimizer_state_dict' in d:
+                self.optimizer.load_state_dict(d['optimizer_state_dict'])
+            if 'scheduler_state_dict' in d:
+                self.scheduler.load_state_dict(d['scheduler_state_dict'])
 
 if __name__ == '__main__':
     from dingo.core.nn.nsf import create_nsf_with_rb_projection_embedding_net
@@ -164,11 +202,17 @@ if __name__ == '__main__':
                       'embedding_net_kwargs': embedding_net_kwargs},
     )
 
+    optimizer_kwargs = {'type': 'adam', 'lr': 0.001}
+    optimizer = torchutils.get_optimizer_from_kwargs(pm.model.parameters(),
+                                                     **optimizer_kwargs)
+
     pm.save_model(model_filename)
 
     pm_loaded = PosteriorModel(
         model_builder=create_nsf_with_rb_projection_embedding_net,
         model_filename=model_filename,
     )
+
+    optimizer = torch.optim.Adam(pm.model.parameters(), lr=0.0001)
 
     print('Done')
