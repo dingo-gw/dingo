d36ca245ffb533ed6b9957b202fa284da70f8686
Michael Pürrer <Michael.Puerrer@aei.mpg.de>
2021-09-29 19:36:54 +0200

    Add logger function borrowed from bilby-pipe

---
+++ dingo/api/utils.py
@@ -0,0 +1,88 @@
+import logging
+import os
+import sys
+
+"""
+Utility functions
+"""
+
+def check_directory_exists_and_if_not_mkdir(directory, logger):
+    """Checks if the given directory exists and creates it if it does not exist
+
+    Parameters
+    ----------
+    directory: str
+        Name of the directory
+
+    Borrowed from bilby-pipe: https://git.ligo.org/lscsoft/bilby_pipe/
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        logger.debug(f"Making directory {directory}")
+    else:
+        logger.debug(f"Directory {directory} exists")
+
+def setup_logger(outdir=None, label=None, log_level="INFO"):
+    """Setup logging output: call at the start of the script to use
+
+    Parameters
+    ----------
+    outdir, label: str
+        If supplied, write the logging output to outdir/label.log
+    log_level: str, optional
+        ['debug', 'info', 'warning']
+        Either a string from the list above, or an integer as specified
+        in https://docs.python.org/2/library/logging.html#logging-levels
+
+    Borrowed from bilby-pipe: https://git.ligo.org/lscsoft/bilby_pipe/
+    """
+
+    if "-v" in sys.argv or "--verbose" in sys.argv:
+        log_level = "DEBUG"
+
+    if isinstance(log_level, str):
+        try:
+            level = getattr(logging, log_level.upper())
+        except AttributeError:
+            raise ValueError(f"log_level {log_level} not understood")
+    else:
+        level = int(log_level)
+
+    logger = logging.getLogger("dingo")
+    logger.propagate = False
+    logger.setLevel(level)
+
+    streams = [isinstance(h, logging.StreamHandler) for h in logger.handlers]
+    if len(streams) == 0 or not all(streams):
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(name)s %(levelname)-8s: %(message)s", datefmt="%H:%M"
+            )
+        )
+        stream_handler.setLevel(level)
+        logger.addHandler(stream_handler)
+
+    if any([isinstance(h, logging.FileHandler) for h in logger.handlers]) is False:
+        if label:
+            if outdir:
+                check_directory_exists_and_if_not_mkdir(outdir, logger)
+            else:
+                outdir = "."
+            log_file = f"{outdir}/{label}.log"
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setFormatter(
+                logging.Formatter(
+                    "%(asctime)s %(levelname)-8s: %(message)s", datefmt="%H:%M"
+                )
+            )
+
+            file_handler.setLevel(level)
+            logger.addHandler(file_handler)
+
+    for handler in logger.handlers:
+        handler.setLevel(level)
+
+
+setup_logger()
+logger = logging.getLogger("dingo")
