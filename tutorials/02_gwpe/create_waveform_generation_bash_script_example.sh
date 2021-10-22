c56caaa8bb86420275e4d07fb2946b179ed16d41
Michael Pürrer <Michael.Puerrer@aei.mpg.de>
2021-10-22 14:53:23 +0200

    Remove obsolete arguments

--- tutorials/02_gwpe/create_waveform_generation_bash_script_example.sh
+++ tutorials/02_gwpe/create_waveform_generation_bash_script_example.sh
@@ -4,10 +4,6 @@ D=/home/mpuer/projects/dingo-devel
 
 $D/env/bin/create_waveform_generation_bash_script \
 --waveforms_directory $D/tutorials/02_gwpe/datasets/waveforms/ \
---parameters_file_basis parameters_basis.npy \
---parameters_file_dataset parameters_dataset.npy \
---basis_file polarization_basis.npy \
---settings_file settings.yaml \
 --dataset_file waveform_dataset.hdf5 \
 --env_path $D/env \
 --num_threads 16
