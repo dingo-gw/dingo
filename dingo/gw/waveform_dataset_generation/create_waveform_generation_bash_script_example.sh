fc7986acec86b2fd729b6f0a3afcf99521d89fee
Michael Pürrer <Michael.Puerrer@aei.mpg.de>
2021-10-22 19:19:05 +0200

    Improve directory names

--- dingo/gw/waveform_dataset_generation/create_waveform_generation_bash_script_example.sh
+++ dingo/gw/waveform_dataset_generation/create_waveform_generation_bash_script_example.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-D=/home/mpuer/projects/dingo-devel
+# Path to a virtual environment in which you have installed dingo
+ENV_DIR=./dingo-devel/venv
+# Path to the directory which contains settings.yaml
+# This is where we will save the waveform data to.
+OUTPUT_DIR=./datasets/waveforms/
 
-$D/env/bin/create_waveform_generation_bash_script \
---waveforms_directory $D/tutorials/02_gwpe/datasets/waveforms/ \
---env_path $D/env \
+$ENV_DIR/bin/create_waveform_generation_bash_script \
+--waveforms_directory $OUTPUT_DIR \
+--env_path $ENV_DIR \
 --num_threads 16
 
