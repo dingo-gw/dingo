5e07946bc8ac47e8b2beabafdb05cf89e7981585
Maximilian Dax <maximilian.dax@tuebingen.mpg.de>
2021-11-04 10:45:43 +0100

    Added documentation for ASDDataset, and a method to truncate the dataset frequency range.

--- dingo/gw/noise_dataset.py
+++ dingo/gw/noise_dataset.py
@@ -2,10 +2,23 @@ import h5py
 import numpy as np
 from scipy.signal import tukey
 import ast
+from dingo.gw.domains import build_domain
 from dingo.gw.gwutils import *
 
 class ASDDataset:
+    """
+    Dataset of amplitude spectral densities (ASDs). The ASDs are typically
+    used for whitening strain data, and additionally passed as context to the
+    neural density estimator.
+    """
     def __init__(self, dataset, ifos=None):
+        """
+        Initializes an ASD dataset by loading the corresponding hdf5 file.
+
+        :param dataset: Path of the hdf5 dataset file.
+        :param ifos:    List of detectors used for dataset, e.g. ['H1', 'L1'].
+                        If not set, all available ones in the dataset are used.
+        """
         with h5py.File(dataset, 'r') as f:
             if ifos is None:
                 ifos = [k[5:] for k in f.keys() if k.startswith('asds_')]
@@ -16,10 +29,48 @@ class ASDDataset:
             self.gps_times = {ifo: f['gps_times'][ifo][:] for ifo in ifos}
             self.metadata = ast.literal_eval(f.attrs['metadata'])
 
-    def truncate_dataset_range(self):
-        raise NotImplementedError
+        self.domain = build_domain(self.metadata['domain_dict'])
+        self.is_truncated = False
+
+    def truncate_dataset_domain(self, new_range = None):
+        """
+        The asd dataset provides asds in the range [0, domain._f_max]. In
+        practice one may want to apply data conditioning different to that of
+        the dataset by specifying a different range, and truncating this
+        dataset accordingly.
+
+        This method provides functionality for that. It truncates the dataset
+        to the range specified by the domain, by calling domain.truncate_data.
+        In uniform FD, this corresponds to truncating data in the range
+        [0, domain._f_max] to the range [domain._f_min, domain._f_max].
+
+        Before this truncation step, one may optionally modify the domain,
+        to set a new range. This is done by domain.set_new_range(*new_range),
+        which is called if new_range is not None.
+        """
+        if self.is_truncated:
+            raise ValueError('Dataset is already truncated')
+        len_domain_original = len(self.domain)
+
+        # optionally set new data range the dataset
+        if new_range is not None:
+            self.domain.set_new_range(*new_range)
+
+        # truncate the dataset
+        for ifo, asds in self.asds.items():
+            assert asds.shape[-1] == len_domain_original, \
+                f'ASDs with shape {self._Vh.shape} are not compatible' \
+                f'with the domain of length {len_domain_original}.'
+            self.asds[ifo] = self.domain.truncate_data(
+                asds, allow_for_flexible_upper_bound=(new_range is not None))
+
+        self.is_truncated = True
 
     def sample_random_asds(self):
+        """
+        Sample a random asd for each detector.
+        :return: Dict with a random asd from the dataset for each detector.
+        """
         return {k: v[np.random.choice(len(v), 1)[0]]
                 for k,v in self.asds.items()}
 
@@ -27,7 +78,6 @@ if __name__ == '__main__':
     # this transforms the np psd datasets from the research code to the hdf5
     # datasets used by dingo
     from os.path import join
-    from dingo.gw.domains import build_domain
     run = 'O1'
     ifos = ['H1', 'L1']
     data_dir = '../../../data/PSDs'
@@ -69,7 +119,6 @@ if __name__ == '__main__':
     noise_std = np.sqrt(window_factor) / \
                 np.sqrt(4*asd_dataset.metadata['domain_dict']['kwargs']['delta_f'])
 
-    from dingo.gw.domains import build_domain
     domain = build_domain(asd_dataset.metadata['domain_dict'])
 
     print(window_factor)
