759dc09a0a8cb1027093c1979f76f9e830e09505
Stephen Green <stephen.green@aei.mpg.de>
2022-01-21 17:04:12 +0100

    Extend save/load from hdf5 to handle pandas DataFrames

--- dingo/core/utils/dataset_utils.py
+++ dingo/core/utils/dataset_utils.py
@@ -1,4 +1,6 @@
 import h5py
+import numpy as np
+import pandas as pd
 
 
 def recursive_hdf5_save(group, d):
@@ -6,8 +8,12 @@ def recursive_hdf5_save(group, d):
         if isinstance(v, dict):
             next_group = group.create_group(k)
             recursive_hdf5_save(next_group, v)
-        else:
+        elif isinstance(v, np.ndarray):
             group.create_dataset(k, data=v)
+        elif isinstance(v, pd.DataFrame):
+            group.create_dataset(k, data=v.to_records(index=False))
+        else:
+            raise TypeError('Cannot save datatype {} as hdf5 dataset.'.format(type(v)))
 
 
 def recursive_hdf5_load(group):
@@ -17,6 +23,9 @@ def recursive_hdf5_load(group):
             d[k] = recursive_hdf5_load(v)
         else:
             d[k] = v[...]
+            # If the array has column names, load it as a pandas DataFrame
+            if d[k].dtype.names is not None:
+                d[k] = pd.DataFrame(d[k])
     return d
 
 
