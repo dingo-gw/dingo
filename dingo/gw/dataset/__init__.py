1e405db3e729f5466fe2db6182416afa55c9311e
Stephen Green <stephen.green@aei.mpg.de>
2022-01-26 18:41:35 +0100

    fix a small bug

--- dingo/gw/dataset/__init__.py
+++ dingo/gw/dataset/__init__.py
@@ -1,3 +1,3 @@
-from utils import merge_datasets
-from generate_dataset import generate_dataset, generate_parameters_and_polarizations
-from waveform_dataset import *
+from .utils import merge_datasets
+from .generate_dataset import generate_dataset, generate_parameters_and_polarizations
+from .waveform_dataset import *
