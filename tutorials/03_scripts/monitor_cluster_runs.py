92dee569c2516b7155e35bc0f0a961f13370405f
Maximilian Dax <maximilian.dax@tuebingen.mpg.de>
2022-03-31 11:43:39 +0200

    Improved script for monitoring cluster runs.

--- tutorials/03_scripts/monitor_cluster_runs.py
+++ tutorials/03_scripts/monitor_cluster_runs.py
@@ -2,10 +2,41 @@ from os.path import join, getmtime, commonpath, relpath, getsize
 import time
 from datetime import timedelta
 from tabulate import tabulate
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description="Monitor cluster runs for dingo")
+parser.add_argument("--condor_name", type=str, default=None,
+                    help="If set, job IDs are fetched via condor_q args.condor_name.")
+args = parser.parse_args()
 
 
-def get_info(train_dir, common_path=""):
+def is_condor_process(row):
+    try:
+        float(row[0])
+        return True
+    except:
+        return False
+
+
+def get_condor_info(train_dir, condor_processes=None):
+    if condor_processes is None:
+        return ""
+    else:
+        condor_process = [pr for pr in condor_processes if train_dir == pr[10]]
+        if len(condor_process) == 0:
+            return ""
+        elif len(condor_process) > 1:
+            IDs = ", ".join([pr[0] for pr in condor_process])
+            print(f"Warning: multiple processes for run {train_dir}: IDs {IDs}.")
+        condor_process = condor_process[0]
+        return " ".join((condor_process[4], condor_process[0], condor_process[5]))
+
+
+def get_info(train_dir, common_path="", condor_processes=None):
     name = relpath(train_dir, common_path)
+    # get condor info
+    condor_info = get_condor_info(train_dir, condor_processes)
     # parse history.txt for epoch, train_loss, lr and time_since_last epoch
     try:
         with open(join(train_dir, "history.txt"), "r") as f:
@@ -42,6 +73,7 @@ def get_info(train_dir, common_path=""):
 
     return [
         name,
+        condor_info,
         timedelta(seconds=int(time_since_last_epoch)),
         time_epoch,
         epoch,
@@ -52,16 +84,29 @@ def get_info(train_dir, common_path=""):
 
 
 with open("cluster_runs.txt", "r") as f:
-    runs = f.read().splitlines()
+    runs = sorted(f.read().splitlines())
 common_path = commonpath(runs)
+print(f"Absolute path: {common_path}\n")
+
+if args.condor_name is not None:
+    condor_processes = subprocess.run(
+        ["condor_q", args.condor_name], stdout=subprocess.PIPE
+    ).stdout.decode("utf-8").split("\n")
+    condor_processes = [pr.split() for pr in condor_processes]
+    condor_processes = [
+        pr for pr in condor_processes if is_condor_process(pr)
+    ]
+else:
+    condor_processes = None
+
 
 table_data = []
 for train_dir in runs:
-    table_data.append(get_info(train_dir, common_path))
+    table_data.append(get_info(train_dir, common_path, condor_processes))
 
-print(f"Absolute path: {common_path}\n")
 headers = [
     "train dir",
+    "condor_info",
     "time since last epoch",
     "time last epoch",
     "epoch",
@@ -69,6 +114,7 @@ headers = [
     "learning rate",
     "error",
 ]
+print("\n")
 print(tabulate(table_data, headers=headers, stralign="right", colalign=("left",)))
 
 # def commonprefix(m):
