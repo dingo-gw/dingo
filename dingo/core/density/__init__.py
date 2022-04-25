79ce5efe3d270abd1158095fad1db03b6eb06c46
Maximilian Dax <maximilian.dax@tuebingen.mpg.de>
2022-04-25 11:37:58 +0200

    Fixed density __init__.py

--- dingo/core/density/__init__.py
+++ dingo/core/density/__init__.py
@@ -3,4 +3,4 @@ This submodule contains tools for density estimation from samples.
 This is required for instance to recover the posterior density from GNPE samples,
 since the density is intractable with GNPE.
 """
-from .density_estimation import train_unconditional_density_estimator
+from .uncogit nditional_density_estimation import train_unconditional_density_estimator
