c3758e184dcad0d2d1d83452e5280c1b9d4299bd
Stephen Green <stephen.green@aei.mpg.de>
2022-04-25 08:21:30 +0200

    Update append_stage to optionally replace a planned training stage.

--- dingo/gw/training/utils.py
+++ dingo/gw/training/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import argparse
 import yaml
@@ -9,6 +10,7 @@ def append_stage():
     parser.add_argument("--checkpoint", type=str, required=True)
     parser.add_argument("--stage_settings_file", type=str, required=True)
     parser.add_argument("--out_file", type=str, required=True)
+    parser.add_argument("--replace", type=int)
     args = parser.parse_args()
 
     # Typically training is done on the GPU, so the model could be saved on a GPU
@@ -19,19 +21,28 @@ def append_stage():
     else:
         d = torch.load(args.checkpoint, map_location=torch.device('cpu'))
 
-    num_stages = len(
-        [
-            k
-            for k in d["metadata"]["train_settings"]["training"]
-            if k.startswith("stage_")
-        ]
-    )
+    stages = [s for s in d["metadata"]["train_settings"]["training"] if s.startswith(
+        "stage_")]
+    num_stages = len(stages)
     print(f"Checkpoint training plan consists of {num_stages} stages.")
 
     with open(args.stage_settings_file, "r") as f:
         new_stage = yaml.safe_load(f)
 
-    d["metadata"]["train_settings"]["training"][f"stage_{num_stages}"] = new_stage
+    if args.replace is not None:
+        current_epoch = d["epoch"]
+        stage_epoch = np.sum(s['epochs'] for s in stages[:args.replace])
+        if current_epoch > stage_epoch:
+            print(f'WARNING: Modification to training plan changes a training stage '
+                  f'that has already started. Current model epoch is {current_epoch}. '
+                  f'Proceed at your own risk!')
+        print(f"Replacing planned stage {args.replace} with new stage.")
+        new_stage_number = args.replace
+    else:
+        print(f"Appending new stage to training plan.")
+        new_stage_number = num_stages
+
+    d["metadata"]["train_settings"]["training"][f"stage_{new_stage_number}"] = new_stage
     print("Summary of new training plan:")
     print(
         yaml.dump(
