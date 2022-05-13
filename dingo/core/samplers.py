Use the correct detector orientation during the likelihood calculation.

dingo/core/samplers.py
@@ -93,12 +93,13 @@ class Sampler(object):
     @property
     def event_metadata(self):
         """Metadata for data analyzed. Can in principle influence any post-sampling
-        parameter transformations (e.g., sky position correction)."""
-        return self.metadata.get("event")
+        parameter transformations (e.g., sky position correction), as well as the
+        likelihood detector positions."""
+        return self.base_model_metadata.get("event")
 
     @event_metadata.setter
     def event_metadata(self, value):
-        self.metadata["event"] = value
+        self.base_model_metadata["event"] = value
 
     def _run_sampler(self, num_samples: int, context: Optional[dict] = None) -> dict:
         if not self.unconditional_model:
@@ -119,8 +120,9 @@ class Sampler(object):
             num_samples = 1
         else:
             if context is not None:
-                raise ValueError("Context should not be passed to an unconditional "
-                                 "sampler.")
+                raise ValueError(
+                    "Context should not be passed to an unconditional " "sampler."
+                )
             x = []
 
         # For a normalizing flow, we get the log_prob for "free" when sampling,
@@ -168,9 +170,7 @@ class Sampler(object):
         if batch_size is None:
             batch_size = num_samples
         full_batches, remainder = divmod(num_samples, batch_size)
-        samples = [
-            self._run_sampler(batch_size, context) for _ in range(full_batches)
-        ]
+        samples = [self._run_sampler(batch_size, context) for _ in range(full_batches)]
         if remainder > 0:
             samples.append(self._run_sampler(remainder, context))
         samples = {p: torch.cat([s[p] for s in samples]) for p in samples[0].keys()}
@@ -195,6 +195,11 @@ class Sampler(object):
         """
         if self.context is None and not self.unconditional_model:
             raise ValueError("Context must be set in order to run sampler.")
+
+        # FIXME: For conditional NPE models analyzing real events, the samples and the
+        #  network will have different t_ref. We must undo the right ascension
+        #  correction here for this case. (This only affects slice plots, and only when
+        #  not using an unconditional model.)
 
         # Standardize the sample parameters and place on device.
         y = samples[self.inference_parameters].to_numpy()
@@ -294,20 +299,45 @@ class Sampler(object):
         )
         print(f"Done. This took {time.time() - t0:.2f} seconds.")
 
-        # Calculate weights.
+        # Calculate weights and normalize them to have mean 1.
         log_weights = log_prior + log_likelihood - log_prob_proposal
         weights = np.exp(log_weights - np.max(log_weights))
         weights /= np.mean(weights)
 
-        # Repackage samples along with weights, etc.
         self.samples = theta
         self.samples["log_prob"] = log_prob_proposal  # Proposal log_prob, not target!
         self.samples["weights"] = weights
         self.samples["log_likelihood"] = log_likelihood
         self.samples["log_prior"] = log_prior
 
-        # When calculating log(evidence), use the *original* number of samples in the
-        # subtraction of log(num_samples).
+        # The evidence
+        #           Z = \int d\theta \pi(\theta) L(\theta),
+        #
+        #                   where   \pi = prior,
+        #                           L = likelihood.
+        #
+        # For importance sampling, we estimate this using Monte Carlo integration using
+        # the proposal distribution q(\theta),
+        #
+        #           Z = \int d\theta q(\theta) \pi(\theta) L(\theta) / q(\theta)
+        #             ~ (1/N) \sum_i \pi(\theta_i) L(\theta_i) / q(\theta_i)
+        #
+        #                   where we are summing over samples \theta_i ~ q(\theta).
+        #
+        # The integrand is just the importance weight (prior to any normalization). It
+        # is more numerically stable to evaluate log(Z),
+        #
+        #           log Z ~ \log \sum_i \exp( log \pi_i + log L_i - log q_i ) - log N
+        #                 = logsumexp ( log_weights ) - log N
+        #
+        # Notes
+        # -----
+        #   * We use the logsumexp functions, which is more numerically stable.
+        #   * N is the *original* number of samples (including the zero-weight ones
+        #   that we dropped).
+        #   * q, \pi, L must be distributions in the same parameter space (the same
+        #   coordinates). We have undone any standardizations so this is the case.
+
         self.log_evidence = logsumexp(log_weights) - np.log(num_samples)
         self.effective_sample_size = np.sum(weights) ** 2 / np.sum(weights ** 2)
 
@@ -373,7 +403,7 @@ class GNPESampler(Sampler):
         super().__init__(model)
         self.init_sampler = init_sampler
         self.num_iterations = num_iterations
-        self.gnpe_parameters = None
+        self.gnpe_parameters = None  # Should be set in subclass.
 
     @property
     def init_sampler(self):
@@ -386,6 +416,7 @@ class GNPESampler(Sampler):
 
     @property
     def num_iterations(self):
+        """The number of GNPE iterations to perform when sampling."""
         return self._num_iterations
 
     @num_iterations.setter

dingo/gw/importance_sampling/importance_weights.py
@@ -2,7 +2,6 @@
 Step 1: Train unconditional nde
 Step 2: Set up likelihood and prior
 """
-import time
 import yaml
 from os import rename, makedirs
 from os.path import dirname, join, isfile, exists
@@ -19,11 +18,7 @@ from dingo.core.models import PosteriorModel
 from dingo.core.samplers import Sampler
 from dingo.core.samples_dataset import SamplesDataset
 from dingo.gw.inference.gw_samplers import GWSamplerUnconditional
-from dingo.gw.likelihood import build_stationary_gaussian_likelihood
 from dingo.core.density import train_unconditional_density_estimator
-from dingo.gw.gwutils import get_extrinsic_prior_dict
-from dingo.gw.prior import build_prior_with_defaults
-from dingo.gw.posterior import UnnormalizedPosterior
 
 
 def parse_args():
@@ -244,28 +239,13 @@ def plot_diagnostics(
     num_processes=1,
     n_grid=200,
 ):
-    # weights = np.array(theta.pop("weights"))
-    # # compute ESS
-    # ESS = get_ESS(weights)
-    # log_probs_proposal = np.array(theta["log_probs_proposal"])
-    # log_probs_target = np.array(theta.pop("log_probs_target"))
-    # print(f"Number of samples:             {len(weights)}")
-    # print(f"Effective sample size (ESS):   {ESS:.0f} ({100 * ESS / len(weights):.2f}%)")
-    # # Compute log_evidence
-    # log_evidence = get_evidence(log_probs_target, log_probs_proposal)
-    # print(f"Log evidence:                  {log_evidence:.2f}")
-    # # Normalize target log_probs
-    # log_probs_target = log_probs_target - log_evidence
-
     theta = sampler.samples
     weights = theta['weights'].to_numpy()
     log_probs_proposal = theta['log_prob'].to_numpy()
 
     log_prior = theta['log_prior'].to_numpy()
     log_likelihood = theta['log_likelihood'].to_numpy()
-    within_prior = log_prior != -np.inf
-    log_probs_target = log_prior.copy()
-    log_probs_target[within_prior] += log_likelihood[within_prior]
+    log_probs_target = log_prior + log_likelihood
 
     ESS = sampler.effective_sample_size
     log_evidence = sampler.log_evidence