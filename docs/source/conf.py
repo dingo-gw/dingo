0c6769c4fade9a3de0275d9fdba0c205d1abe636
Michael Pürrer <mpuerrer@users.noreply.github.com>
2023-03-06 08:53:36 -0500

    Replace setup.py with pyproject.toml (#140)
    * Replace setup.py with pyproject.toml
    
    Set up for building and uploading to PyPI.
    
    * Bump setuptools version and tweak README
    
    Setuptools v62.0.0 or newer might be needed. This is covered by igwn-testing
    https://stackoverflow.com/questions/66857302/how-does-one-install-a-setup-cfg-pyproject-toml-python-project-in-editable-mod
    
    Removed `python setup.py bdist_wheel`. There is no setup.py any longer. Just `pip install -e ."[dev]"` built and installed the package locally.
    
    * Add packages for buildings docs to "dev"
    
    * Add a separate file listing all authors
    
    AFAIK, this cannot be ingested by pyproject.toml, so the authors
    listed in this file and pyproject.toml are separate.
    
    * Add missing scripts and tweak description.
    
    * Add dynamic versioning with setuptools_scm, change Python version.
    
    * Automatic package discovery.
    
    * Exclude misc_scripts, .gitignore from source distribution.
    
    * Move condor_wrapper.py to condor_utils in core, and remove api package.
    
    * Dynamic versioning for docs
    
    * Remove deprecated package sklearn.
    
    * Update .gitignore
    
    * Re-organize author list
    
    * Update author list
    
    ---------
    
    Co-authored-by: Stephen Green <stephenrolandgreen@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,18 +12,18 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
+from importlib.metadata import version as _version
 sys.path.insert(0, os.path.abspath('../../dingo'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'dingo'
-copyright = '2022, Stephen Green'
+project = 'dingo-gw'
+copyright = '2022-2023, Stephen Green'
 author = 'Stephen Green'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+version = _version('dingo-gw')
 
 
 # -- General configuration ---------------------------------------------------
