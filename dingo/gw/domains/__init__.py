260524dda132c2440f526a79a11b75a3b96e3d8a
Maximilian Dax <maximilian.dax@tuebingen.mpg.de>
2023-04-11 07:05:11 +0200

    Moved determination of mfd bands into domain builder.

--- dingo/gw/domains/__init__.py
+++ dingo/gw/domains/__init__.py
@@ -1,9 +1,13 @@
 from .base import Domain
 from .frequency_domain import FrequencyDomain
 from .time_domain import TimeDomain
-from .multibanded_frequency_domain import MultibandedFrequencyDomain
+from .multibanded_frequency_domain import (
+    MultibandedFrequencyDomain,
+    get_decimation_bands_adaptive,
+)
 
 from copy import deepcopy
+import numpy as np
 
 
 def build_domain(settings: dict) -> Domain:
@@ -67,29 +71,45 @@ def build_domain_from_wfd_settings(settings: dict, num_processes: int = 0) -> Do
         settings["domain"]["type"] == "MultibandedFrequencyDomain"
         and "bands" not in settings["domain"]
     ):
-        from dingo.gw.dataset import generate_dataset
+        settings_mfd = deepcopy(settings["domain"])
+        settings_mfd.pop("type")
+        multibanding_method = settings_mfd.pop("multibanding_method", "adaptive")
 
-        settings_new = deepcopy(settings)
-        settings_new["num_samples"] = settings["domain"]["num_samples_band_generation"]
-        settings_new["domain"] = settings["domain"]["base_domain"]
-        # We don't need compression to generate just a few waveforms for this step.
-        if "compression" in settings_new:
-            settings_new["compression"].pop("svd", None)
-        wfd = generate_dataset(settings_new, num_processes)
-        # We now extract the polarizations directly, and not via wfd[idx] (similarly to
-        # what's done in train_svd_basis in dingo.gw.dataset.generate_datset). This means
-        # that decompression transforms (in particular, phase_heterodyning) will *not*
-        # be applied, which is desirable if the final waveform passed to the network
-        # will be transformed in a similar way (e.g., GNPE for phase heterodyning).
-        mfd = MultibandedFrequencyDomain.init_from_polarizations(
-            polarizations=wfd.polarizations,
-            **{
-                k: v
-                for k, v in settings["domain"].items()
-                if k not in ["type", "num_samples_band_generation"]
-            },
-        )
+        if multibanding_method == "adaptive":
+            from dingo.gw.dataset import generate_dataset
+
+            # Generate polarizations in uniform FrequencyDomain
+            print(
+                "Generating polarizations to determine suitable bands for the "
+                "MultibandedFrequencyDomain."
+            )
+            settings_ufd = deepcopy(settings)
+            settings_ufd["num_samples"] = settings_mfd.pop(
+                "num_samples_band_generation"
+            )
+            settings_ufd["domain"] = settings_mfd.pop("base_domain")
+            # We don't need compression to generate just a few waveforms for this step.
+            if "compression" in settings_ufd:
+                settings_ufd["compression"].pop("svd", None)
+            wfd = generate_dataset(settings_ufd, num_processes)
+            # We now extract the polarizations directly, and not via wfd[idx] (similarly
+            # to what's done in train_svd_basis in dingo.gw.dataset.generate_datset).
+            # This means that decompression transforms (in particular phase_heterodyning)
+            # will *not* be applied, which is desirable if the final waveform passed to
+            # the network will be transformed in a similar way (e.g., GNPE for phase
+            # heterodyning).
+            polarizations = np.concatenate(list(wfd.polarizations.values()))
+
+            bands = get_decimation_bands_adaptive(
+                wfd.domain, polarizations, **settings_mfd
+            )
+        else:
+            raise NotImplementedError(
+                f"Unknown multibanding method {multibanding_method}."
+            )
+        mfd = MultibandedFrequencyDomain(bands, wfd.domain)
         settings["domain"] = mfd.domain_dict
+        print(f"MultibandedFrequencyDomain bands:\n{mfd.bands}")
         return mfd
 
     else:
