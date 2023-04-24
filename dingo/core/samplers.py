f258f6c261745e535eccef07362a2ddc9e9b850f
Stephen Green <stephenrolandgreen@gmail.com>
2023-04-24 16:40:09 +0100

    Move IterationTracker to samplers.py

--- dingo/core/samplers.py
+++ dingo/core/samplers.py
@@ -8,12 +8,14 @@ import sys
 import numpy as np
 import pandas as pd
 import torch
+from scipy.stats import kstest
 from torchvision.transforms import Compose
 
+from dingo.core.dataset import DingoDataset
 from dingo.core.models import PosteriorModel
 from dingo.core.result import Result
 from dingo.core.result import DATA_KEYS as RESULT_DATA_KEYS
-from dingo.core.utils import torch_detach_to_cpu, IterationTracker
+from dingo.core.utils import torch_detach_to_cpu
 
 # FIXME: transform below should be in core
 from dingo.gw.transforms import SelectStandardizeRepackageParameters
@@ -585,3 +587,69 @@ class GNPESampler(Sampler):
                 assert torch.equal(proxies[k], init_proxies[k])
 
         return samples
+
+
+class IterationTracker:
+    def __init__(self, data=None, store_data=False):
+        self.data = data
+        self.ks_result = None
+        self.store_data = store_data
+
+    def update(self, new_data):
+        """
+        Append new_data to self.data.
+
+        Parameters
+        ----------
+        new_data: dict
+            dict with numpy arrays to append to data
+
+        Returns
+        -------
+
+        """
+        if self.data is None:
+            self.data = {k: v.copy()[None, :] for k, v in new_data.items()}
+
+        else:
+            x = {k: v[-1, :] for k, v in self.data.items()}
+            y = new_data
+            # get ks
+            statistic, pvalue = [], []
+            for k in y.keys():
+                N = len(x[k]) // 2
+                ks_result = kstest(x[k][:N], y[k][N:])
+                statistic.append(ks_result[0])
+                pvalue.append(ks_result[1])
+            self.ks_result = {"statistics": statistic, "pvalue": pvalue}
+
+            if not self.store_data:
+                self.data = {k: v.copy()[None, :] for k, v in y.items()}
+            else:
+                self.data = {
+                k: np.concatenate((v, y[k][None, :]), axis=0)
+                for k, v in self.data.items()
+            }
+
+    @property
+    def pvalue_min(self):
+        if self.ks_result is None:
+            return -np.inf
+        else:
+            return min(self.ks_result["pvalue"])
+
+    def mean(self, idx):
+        return {k: self.data[k][idx].mean() for k in self.data}
+
+    def std(self, idx):
+        return {k: self.data[k][idx].std() for k in self.data}
+
+    def save(self, filename):
+        dataset = DingoDataset(dictionary={"parameters": self.data}, data_keys=[
+            "parameters"])
+        dataset.to_file(filename)
+
+    # def remove_outliers(self, x):
+    #     xc = np.concatenate([v[None, :] for v in self.x.values()], axis=0)
+    #     yc = np.concatenate([v[None, :] for v in y.values()], axis=0)
+    #     gaussian_kde(yc).logpdf(yc)
