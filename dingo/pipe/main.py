29bc8b803c63c462abfb7d8faaf6628028240d97
Nihar Gupte <ngupte@umd.edu>
2023-06-24 15:40:01 +0200

    main ast.literal_eval fix

--- dingo/pipe/main.py
+++ dingo/pipe/main.py
@@ -169,7 +169,8 @@ class MainInput(BilbyMainInput):
         # self.zero_noise = args.zero_noise
         # self.n_simulation = args.n_simulation
         self.injection_file = args.injection_file
-        self.injection_parameters = ast.literal_eval(args.injection_parameters)
+        if args.injection_parameters is not None:
+            self.injection_parameters = ast.literal_eval(args.injection_parameters)
 
         self.importance_sample = args.importance_sample
 
