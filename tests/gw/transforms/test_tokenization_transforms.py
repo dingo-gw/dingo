eea4bd572a17529ad93a8fba657b90ac529902a1
Annalena Kofler <annalena.kofler@tuebingen.mpg.de>
2024-01-12 16:13:09 +0100

    reformatted tokenization transform and test

--- tests/gw/transforms/test_tokenization_transforms.py
+++ tests/gw/transforms/test_tokenization_transforms.py
@@ -5,20 +5,23 @@ from dingo.gw.transforms import StrainTokenization
 
 def test_StrainTokenization():
     num_tokens = 20
-    f_min = 20.
-    f_max = 1024.
-    T = 8.
-    num_f = int((f_max - f_min)*T) + 1
+    f_min = 20.0
+    f_max = 1024.0
+    T = 8.0
+    num_f = int((f_max - f_min) * T) + 1
 
-    token_transformation = StrainTokenization(num_tokens, f_min, f_max, df=1/T)
+    token_transformation = StrainTokenization(num_tokens, f_min, f_max, df=1 / T)
     waveform = np.random.random_sample([2, 3, num_f])
-    asds = {'H1': np.random.random_sample(num_f),
-            'L1': np.random.random_sample(num_f)}
+    asds = {"H1": np.random.random_sample(num_f), "L1": np.random.random_sample(num_f)}
     sample = {"waveform": waveform, "asds": asds}
 
     out = token_transformation(sample)
     # Check that waveform has expected shape
-    assert out["waveform"].shape[:-1] == (waveform.shape[0], waveform.shape[1], num_tokens)
+    assert out["waveform"].shape[:-1] == (
+        waveform.shape[0],
+        waveform.shape[1],
+        num_tokens,
+    )
     # Check that token parameters have expected shape
     assert out["f_min_per_token"].shape == out["f_max_per_token"].shape
     assert len(out["f_min_per_token"]) == num_tokens
