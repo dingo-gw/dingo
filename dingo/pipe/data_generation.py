934e3bef62c50f7f3494ec5dd513035857e69e2f
Nihar Gupte <ngupte@umd.edu>
2024-02-19 16:25:17 +0100

    hot fix to change args.gaussian_noise to self.gaussian_noise

--- dingo/pipe/data_generation.py
+++ dingo/pipe/data_generation.py
@@ -156,8 +156,8 @@ class DataGenerationInput(BilbyDataGenerationInput):
             args.injection_dict = None
             args.injection_waveform_arguments = None
             args.injection_frequency_domain_source_model = None
+            args.gaussian_noise = False
             self.frequency_domain_source_model = None
-            self.gaussian_noise = False
 
             self.create_data(args)
 
