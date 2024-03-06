b6d40819b70c85b513f6614ca6caa0e1fae0aa56
Annalena Kofler <annalena.kofler@tuebingen.mpg.de>
2024-03-06 10:39:52 +0100

    include code from PR fix_dingo_pipe_simple_submission

--- dingo/pipe/utils.py
+++ dingo/pipe/utils.py
@@ -3,6 +3,6 @@ def _strip_unwanted_submission_keys(job):
     job.universe = None
     extra_lines = []
     for line in job.extra_lines:
-        if not line.startswith("priority") and not line.startswith("accounting_group"):
+        if not line.startswith("priority") and not line.startswith("accounting_group") and not line.startswith("ENV"):
             extra_lines.append(line)
     job.extra_lines = extra_lines
