9b0101bfc3d46c0ecdcb675892a9ccd79c14c081
Nihar Gupte <ngupte@umd.edu>
2024-04-05 12:24:44 +0200

    reduced tolerance on test_FD_time_translation_torch

--- tests/gw/test_domains.py
+++ tests/gw/test_domains.py
@@ -160,7 +160,7 @@ def test_FD_time_translation_torch(uniform_FD_params):
     result = domain.time_translate_data(data, -dt)
     assert result.dtype == torch.float32
     assert result.shape == data.shape
-    assert torch.allclose(result[..., 0, :], torch.tensor(1.0), atol=1e-5)
+    assert torch.allclose(result[..., 0, :], torch.tensor(1.0), atol=1e-4)
     # Tolerance of 1e-2 is required in the imaginary part, likely because we are
     # checking that it *vanishes*. This is a consequence of single-precision floats.
     # TODO: Is there a way to improve on this?
