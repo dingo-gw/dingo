e781a4545ad313fb6b2ea66d5ada1afa274b04b5
Konstantin Leyde <konstantin.leyde@ligo.org>
2024-10-17 15:30:22 +0100

    implement vectorized selection of events

--- dingo/populations/models_training/population_model.py
+++ dingo/populations/models_training/population_model.py
@@ -33,6 +33,101 @@ from dingo.gw.transforms import (
 import time
 from threadpoolctl import threadpool_limits
 
+def check_below_required_event_numbers(n_lim_eff, n_lim):
+
+    """
+    Check whether there are enough detected events. Takes as input a 
+    one-dimension tensor n_lim_eff of shape (batch size)
+    
+    Parameters
+    ----------
+    n_lim_eff : torch.Tensor
+        Two-dimension tensor of shape (batch size, number of events).
+    n_lim : int
+        Required number of events.
+    
+    Returns
+    -------
+    bool
+        True if there are enough detected events, False otherwise.
+
+    """
+
+    return torch.any(n_lim_eff != n_lim)
+
+def limit_ones_vectorized(tensor, n_lim, maximum_population_size):
+
+    """
+    This function takes as input a tensor of shape (batch size, number of events) and a tensor
+    of shape (batch size) that specifies the number of events that were drawn.
+    Finally, maximum_population_size defines the maximum number of events that can be drawn.
+
+    The function returns two tensors: 
+
+    (1) A tensor of shape (batch size, maximum_population_size) where per batch we have exactly
+    n_lim ones. I.e. we cut all ones per batch if they exceed n_lim.
+
+    (2) A tensor of shape (batch size, maximum_population_size) where per batch we have exactly
+    maximum_population_size ones. I.e. we add ones so we have exactly maximum_population_size ones
+
+    Parameters
+    ----------
+    tensor : torch.Tensor
+        Tensor of shape (batch size, number of events).
+    n_lim : torch.Tensor
+        Tensor of shape (batch size).
+    maximum_population_size : int
+
+    Returns
+    -------
+    limited_tensor : torch.Tensor
+        Tensor of shape (batch size, maximum_population_size).
+    limited_tensor2 : torch.Tensor
+        Tensor of shape (batch size, maximum_population_size).
+
+    
+    """
+
+    if torch.any(n_lim > maximum_population_size):
+        raise 'Cannot produce more event than the maximum number of events. '
+
+    device = tensor.device
+
+    start_time = time.time()
+    times = {}
+    
+    # Get the shape of the input tensor
+    num_batch, num_events = tensor.size()
+
+    tensor_int = (~tensor).int()
+    
+    # Get the indices where the True values are located, sorted along the rows
+    sorted_indices = torch.argsort(tensor_int, dim=1, descending=False)
+
+    n_detected_per_pop = tensor.sum(axis=1)
+
+    print('Minimum events: ', torch.min(n_detected_per_pop))
+
+    n_lim.to(device)
+    n_lim_eff = torch.minimum(n_lim, n_detected_per_pop)
+
+    # make sure there are enough detected embeddings
+    if check_below_required_event_numbers(n_lim_eff, n_lim):
+        print("Not enough detected embeddings.")
+    
+    # Create an index mask that selects the first n_lim ones for each row
+    mask = (torch.arange(num_events).expand(num_batch, num_events).to(device) < n_lim_eff[:,None])
+    mask2 = (torch.arange(num_events).expand(num_batch, num_events).to(device) < maximum_population_size)
+
+    # Use the sorted indices to mask out the extra ones
+    limited_tensor = torch.zeros_like(mask)
+    limited_tensor2 = torch.zeros_like(mask2)
+
+    limited_tensor.scatter_(1, sorted_indices, mask)
+    limited_tensor2.scatter_(1, sorted_indices, mask2)
+
+    return limited_tensor, limited_tensor2
+
 def get_detected_embeddings(x, embedding_emulator, selection_model, number_events, maximum_population_size):
 
     """ 
@@ -57,29 +152,37 @@ def get_detected_embeddings(x, embedding_emulator, selection_model, number_event
         Detected embeddings.
     
     """
-    
+
+    time_start = time.time()
+    train_time = {}
+
     # sample from embedding emulator
     emb = embedding_emulator.sample(x=x)
+
+    num_batch, num_events, _ = emb.size()
+
+    train_time['compute_embeddings'] = time.time() - time_start
 
     # check whether embeddings are detected
     idx = selection_model.apply_selection_to_embeddings(emb)
 
-    # make sure there are enough detected embeddings
-    not_enough_events = check_below_required_event_numbers(idx, number_events)
+    train_time['compute_selection'] = time.time() - train_time['compute_embeddings'] - time_start
 
-    if(torch.any(not_enough_events)):
-        print("We have generated event numbers: ", idx.sum(axis=1))
-        print("We need the following event numbers: ", number_events)
-        # raise ValueError("Not enough detected embeddings.")
+    # cut all detections above the required number of events
+    idx, idx2 = limit_ones_vectorized(idx, number_events, maximum_population_size)
+
+    train_time['compute_idx'] = time.time() - train_time['compute_selection'] - time_start
+
+    # mask embeddings
+    emb[~idx] = 0
+
+    train_time['set_emb_to_zero'] = time.time() - train_time['compute_idx'] - time_start
+
+    # for k in train_time.keys():
+    #     print(f"{k}: {train_time[k]}")
 
     # only return detected embeddings
-    emb_det = torch.zeros((emb.shape[0], maximum_population_size, emb.shape[2]))
-
-    for i,v in enumerate(emb):
-        # collate zeros for missing events, but should never happen, 
-        # because we check above if we have enough events
-        vv = v[idx[i]]
-        emb_det[i,:number_events[i]] = vv[:number_events[i]]
+    emb_det = emb[idx2].view(num_batch, maximum_population_size, -1)
 
     return emb_det
 
@@ -126,11 +229,13 @@ def train_epoch_population_model(pm, dataloader):
 
 def test_epoch_population_model(pm, dataloader):
 
-    pm_embedding_emulator = pm.pm_embedding_emulator
-    selection_model = pm.selection_model
+    pm_embedding_emulator = pm.embedding_emulator
+    selection_model = pm.snr_estimator
 
     pm_embedding_emulator.model.eval()
-    selection_model.model.eval()    
+    selection_model.model.eval()
+
+    maximum_population_size = pm.metadata["train_settings"]["data"]["maximum_population_size"]
 
     with torch.no_grad():
         pm.model.eval()
@@ -160,27 +265,24 @@ def test_epoch_population_model(pm, dataloader):
 
         return loss_info.get_avg()
 
-def check_below_required_event_numbers(idx, number_min):
+def check_below_required_event_numbers(n_lim_eff, n_lim):
 
     """
     Check whether there are enough detected events. Takes as input a 
-    two-dimension tensor idx of shape (batch size, number of events)
+    one-dimension tensor n_lim_eff of shape (batch size)
     
     Parameters
     ----------
-    idx : torch.Tensor
+    n_lim_eff : torch.Tensor
         Two-dimension tensor of shape (batch size, number of events).
-    number_min : int
-        Minimum number of detected events.
+    n_lim : int
+        Required number of events.
     
     Returns
     -------
     bool
         True if there are enough detected events, False otherwise.
-        shape: (batch size)
 
     """
 
-    num_events_det = idx.sum(axis=1)
-    
-    return num_events_det < number_min
+    return torch.any(n_lim_eff != n_lim)
