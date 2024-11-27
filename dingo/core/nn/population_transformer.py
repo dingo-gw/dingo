0c3814a1cd42295b87fa772db1ea59f9f99e876d
Konstantin Leyde <konstantin.leyde@ligo.org>
2024-11-27 15:24:36 +0000

    implement variance option

--- dingo/core/nn/population_transformer.py
+++ dingo/core/nn/population_transformer.py
@@ -7,11 +7,12 @@ from dingo.core.nn.enets import DenseResidualNet
 
 
 class PopulationTransformer(nn.Module):
-    def __init__(self, tokenizer, transformer_encoder, final_net=None):
+    def __init__(self, tokenizer, transformer_encoder, final_net=None, use_variance=False):
         super().__init__()
         self.tokenizer = tokenizer
         self.transformer_encoder = transformer_encoder
         self.final_net = final_net
+        self.use_variance = use_variance
 
         self.init_weights()
 
@@ -35,9 +36,25 @@ class PopulationTransformer(nn.Module):
         # Average over non-masked components.
         if src_key_padding_mask is not None:
             denominator = torch.sum(~src_key_padding_mask, -1, keepdim=True)
-            x = torch.sum(x * ~src_key_padding_mask.unsqueeze(-1), dim=-2) / denominator
+            
+            if self.use_variance:
+                x1 = torch.sum(x * ~src_key_padding_mask.unsqueeze(-1), dim=-2) / denominator
+                # Compute variance
+                x2 = torch.sum((x ** 2) * ~src_key_padding_mask.unsqueeze(-1), dim=-2) / denominator
+                # Concatenate mean and variance
+                x = torch.cat([x1, x2], axis=-1)
+            else:
+                x = torch.sum(x * ~src_key_padding_mask.unsqueeze(-1), dim=-2) / denominator
+
         else:
-            x = torch.mean(x, dim=-2)
+            if self.use_variance:
+                x1 = torch.mean(x, dim=-2)
+                # Compute variance
+                x2 = torch.mean(x ** 2, dim=-2)
+                # Concatenate mean and variance
+                x = torch.cat([x1, x2], axis=-1)
+            else:
+                x = torch.mean(x, dim=-2)
 
         if self.final_net is not None:
             x = self.final_net(x)
@@ -51,7 +68,13 @@ def create_population_transformer(config):
         config = SimpleNamespace(**config)
     config.tokenizer["input_dim"] = config.d_dingo_encoding
     config.tokenizer["output_dim"] = config.transformer["d_model"]
-    config.final_net["input_dim"] = config.transformer["d_model"]
+
+    use_variance = config.transformer.get("use_variance", False)
+
+    if(use_variance):
+        config.final_net["input_dim"] = 2*config.transformer["d_model"]
+    else:
+        config.final_net["input_dim"] = config.transformer["d_model"]
 
     # build individual modules
     tokenizer = DenseResidualNet(**config.tokenizer)
@@ -67,5 +90,5 @@ def create_population_transformer(config):
     )
     final_net = DenseResidualNet(**config.final_net)
 
-    encoder = PopulationTransformer(tokenizer, transformer, final_net)
+    encoder = PopulationTransformer(tokenizer, transformer, final_net, use_variance=use_variance)
     return encoder
