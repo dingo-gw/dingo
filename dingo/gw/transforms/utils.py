dd43ab811a37be1392cd787d4064904841a996b9
Nihar Gupte <ngupte@umd.edu>
2024-12-31 12:05:21 +0100

    added documentation to get_batch_size_of_input_sample

--- dingo/gw/transforms/utils.py
+++ dingo/gw/transforms/utils.py
@@ -1,4 +1,21 @@
-def get_batch_size_of_input_sample(input_sample):
+from typing import Tuple
+
+
+def get_batch_size_of_input_sample(input_sample: dict) -> Tuple[bool, int]:
+    """
+    Parameters
+    ----------
+    input_sample : dict
+        The input sample to transform. Can contain keys of
+        parameters, polarizations
+
+    Returns
+    -------
+    Tuple[bool, int]
+        A tuple containing a boolean indicating if the input sample is batched
+        and an integer representing the batch size of the input sample
+
+    """
     if "parameters" in input_sample:
         params = next(iter(input_sample["parameters"].values()))
 
@@ -11,3 +28,8 @@ def get_batch_size_of_input_sample(input_sample):
                 return True, 1
             else:
                 return True, params.shape[0]
+    else:
+        raise NotImplementedError(
+            """The parameter key is used to determine the batch size of the
+            input sample. If you want to determine the batch size a different
+            way, you can implement it here.""" )
