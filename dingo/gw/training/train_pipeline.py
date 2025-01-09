976c150881978fe7292e320e5b1486eb465482bf
Annalena Kofler <annalena.kofler@tuebingen.mpg.de>
2025-01-09 16:05:16 +0100

    refactor code such that multi-GPU training is also possible for dingo_train and not only dingo_train_condor

--- dingo/gw/training/train_pipeline.py
+++ dingo/gw/training/train_pipeline.py
@@ -1,9 +1,14 @@
-import os
-import numpy as np
-import yaml
 import argparse
+import numpy as np
+import os
+import queue
+import sys
 import textwrap
+import yaml
 
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn.modules import Module
 
 from threadpoolctl import threadpool_limits
 
@@ -23,7 +28,13 @@ from dingo.core.utils import (
 )
 from dingo.core.utils.trainutils import EarlyStopping, RuntimeLimits
 from dingo.core.utils.environment import document_environment
-from dingo.core.utils.torchutils import document_gpus
+from dingo.core.utils.torchutils import (
+    document_gpus,
+    setup_ddp,
+    cleanup_ddp,
+    replace_BatchNorm_with_SyncBatchNorm,
+    set_seed_based_on_rank,
+)
 from dingo.gw.dataset import WaveformDataset
 from dingo.gw.training.train_builders import (
     build_dataset,
@@ -59,7 +70,7 @@ def prepare_wfd_and_initialization_for_embedding_network(
 
     Returns
     -------
-    (WaveformDataset, BasePosteriorModel)
+    (WaveformDataset, initial_weights, BasePosteriorModel)
     """
 
     # No transforms yet
@@ -259,10 +270,10 @@ def prepare_training_new(
         initial_weights,
         pretrained_embedding_net,
     ) = prepare_wfd_and_initialization_for_embedding_network(
-        train_settings,
-        train_dir,
-        local_settings,
-        pretraining,
+        train_settings=train_settings,
+        train_dir=train_dir,
+        local_settings=local_settings,
+        pretraining=pretraining,
     )
     pm = prepare_model_new(
         train_settings,
@@ -579,9 +590,10 @@ def train_stages(pm, wfd, train_dir, local_settings):
             try:
                 early_stopping = EarlyStopping(**stage["early_stopping"])
             except Exception:
-                print("Early stopping settings invalid. Please pass 'patience', 'delta', 'metric'")
+                print(
+                    "Early stopping settings invalid. Please pass 'patience', 'delta', 'metric'"
+                )
                 raise
-
 
         runtime_limits.max_epochs_total = end_epochs[n]
         pm.train(
@@ -618,6 +630,294 @@ def train_stages(pm, wfd, train_dir, local_settings):
         return True
     else:
         return False
+
+
+def run_training(
+    train_settings: dict,
+    local_settings: dict,
+    train_dir: str,
+    ckpt_file: str,
+    resume: bool,
+    pretraining: bool = False,
+) -> (bool, int):
+    """
+    Starts a training run for single-GPU training.
+
+    Parameters
+    ----------
+    train_settings: dict
+        Settings for training
+    local_settings: dict
+        Local settings
+    train_dir: str
+        Directory to store training output
+    ckpt_file: str
+        if resume=True, path to the model checkpoint
+    resume: bool
+        Whether to resume training from checkpoint
+    pretraining: bool=False
+        Whether to run pretraining
+
+    Returns
+    ----------
+    complete: bool
+        Whether the training run was successful
+    epoch: int
+        The epoch number where the training finished
+    """
+    if not resume:
+        pm, wfd = prepare_training_new(train_settings, train_dir, local_settings)
+    else:
+        pm, wfd = prepare_training_resume(
+            checkpoint_name=ckpt_file,
+            local_settings=local_settings,
+            train_dir=train_dir,
+        )
+    with threadpool_limits(limits=1, user_api="blas"):
+        complete = train_stages(pm, wfd, train_dir, local_settings)
+
+    return complete, pm.epoch
+
+
+def run_multi_gpu_training(
+    world_size: int,
+    train_settings: dict | None,
+    local_settings: dict,
+    train_dir: str,
+    ckpt_file: str,
+    resume: bool,
+    pretraining: bool = False,
+) -> (bool, int):
+    """
+    Starts a training run for multi-GPU distributed data parallel (DDP) training.
+
+    Parameters
+    ----------
+    world_size: int
+        The number of GPUs to use for DDP training.
+    train_settings: dict
+        Settings for training
+    local_settings: dict
+        Local settings
+    train_dir: str
+        Directory to store training output
+    ckpt_file: str
+        if resume=True, path to the model checkpoint
+    resume: bool
+        Whether to resume training from checkpoint
+    pretraining: bool=False
+        Whether to run pretraining
+
+    Returns
+    ----------
+    complete: bool
+        Whether the training run was successful
+    epoch: int
+        The epoch number where the training finished
+    """
+
+    initial_weights, pretrained_emb_net, checkpoint_file = None, None, None
+    if not resume:
+        (
+            wfd,
+            initial_weights,
+            pretrained_emb_net,
+        ) = prepare_wfd_and_initialization_for_embedding_network(
+            train_settings, train_dir, local_settings
+        )
+    else:
+        checkpoint_file = os.path.join(train_dir, ckpt_file)
+        train_settings = load_settings_from_ckpt(ckpt_file)
+        wfd = build_dataset(
+            train_settings["data"],
+            copy_to_tmp=local_settings["copy_waveform_dataset_to_tmp"],
+        )
+    if pretrained_emb_net is not None:
+        pretraining = True
+    else:
+        pretraining = False
+
+    # Setup multi-processing queue
+    result_queue = mp.Queue()
+    processes = []
+    # Start one process for each gpu
+    for rank in range(world_size):
+        p = mp.Process(
+            target=run_training_ddp,
+            args=(
+                rank,
+                world_size,
+                train_settings,
+                local_settings,
+                train_dir,
+                wfd,
+                initial_weights,
+                pretraining,
+                pretrained_emb_net,
+                checkpoint_file,
+                resume,
+                result_queue,
+            ),
+        )
+        p.start()
+        processes.append(p)
+
+    # Monitor the processes
+    error_occurred = False
+    for p in processes:
+        # Force parent process to wait for the subprocess to complete/exit with error
+        p.join()
+        # Check whether process p failed
+        if p.exitcode != 0:
+            error_occurred = True
+            print(
+                f"Process {p.pid} failed with exit code {p.exitcode}. Aborting all processes."
+            )
+            # Terminate all other processes
+            for proc in processes:
+                if proc.is_alive():
+                    proc.terminate()
+            break
+
+    # Handle the result queue to see what the specific error was
+    results = []
+    try:
+        while True:
+            temp_result = result_queue.get_nowait()  # Avoid blocking
+            results.append(temp_result)
+            if temp_result[1] is False:
+                print(f"Rank {temp_result[0]} failed with error: {temp_result[3]}")
+            else:
+                print(f"Rank {temp_result[0]} completed successfully.")
+    except queue.Empty:
+        pass
+
+    if error_occurred or len(results) != world_size:
+        raise RuntimeError(
+            f"One or more processes failed or number of results={len(results)} does not match "
+            f"world_size={world_size}, check info.out for details."
+        )
+
+    # Collect exit results from all processes after training
+    complete, pm_epoch = [], []
+    for result in results:
+        complete.append(result[1])
+        pm_epoch.append(result[2])
+    assert all(complete) is True, f"Not all processes exited successfully: {complete}."
+    assert (
+        len(set(pm_epoch)) == 1
+    ), f"Processes do not return the same epochs: {pm_epoch}."
+
+    return complete[0], pm_epoch[0]
+
+
+def run_training_ddp(
+    rank: int,
+    world_size: int,
+    train_settings: dict,
+    local_settings: dict,
+    train_dir: str,
+    wfd: WaveformDataset,
+    initial_weights: dict,
+    pretraining: bool,
+    pretrained_emb_net: Module,
+    ckpt_file: str,
+    resume: bool,
+    result_queue: mp.Queue = None,
+) -> (bool, int):
+    """
+    Initializes each GPU process of the distributed data parallel (DDP) training.
+
+    Parameters
+    ----------
+    rank: int
+        The rank of the current GPU process.
+    world_size: int
+        The number of GPUs to use for DDP training.
+    train_settings: dict
+        Settings for training
+    local_settings: dict
+        Local settings
+    train_dir: str
+        Directory to store training output
+    wfd: WaveformDataset
+        Waveform dataset used for training
+    initial_weights: dict
+        Initial weights for embedding network
+    pretraining: bool
+        Whether to use the pretrained embedding network
+    pretrained_emb_net: Module
+        Pretrained embedding network
+    ckpt_file: str
+        if resume=True, path to the model checkpoint
+    resume: bool
+        Whether to resume training from checkpoint
+    result_queue:
+        The queue to which the results of the run will be stored
+
+    Returns
+    ----------
+    complete: bool
+        Whether the training run was successful
+    epoch: int
+        The epoch number where the training finished
+    """
+    try:
+        # Initialize process group
+        setup_ddp(rank, world_size)
+        # Set seeds for each GPU to different value
+        set_seed_based_on_rank(rank)
+
+        # Document GPUs
+        if rank == 0:
+            document_gpus(train_dir)
+
+        local_settings["device"] = f"cuda:{rank}"
+        local_settings["rank"] = rank
+        local_settings["world_size"] = world_size
+
+        if not resume:
+            pm = prepare_model_new(
+                train_settings,
+                train_dir,
+                local_settings,
+                wfd=wfd,
+                initial_weights=initial_weights,
+                pretraining=pretraining,
+                pretrained_embedding_net=pretrained_emb_net,
+            )
+        else:
+            pm = prepare_model_resume(
+                checkpoint_name=ckpt_file,
+                local_settings=local_settings,
+                train_dir=train_dir,
+            )
+
+        # Replace BatchNorm layers with SyncBatchNorm
+        pm.network = replace_BatchNorm_with_SyncBatchNorm(pm.network)
+        # Wrap the model with DDP
+        pm.network = DDP(pm.network, device_ids=[rank])
+
+        complete = train_stages(pm, wfd, train_dir, local_settings)
+
+        if complete and local_settings.get("wandb", False) and rank == 0.0:
+            try:
+                import wandb
+
+                wandb.finish()
+            except ImportError:
+                print("wandb not installed. Skipping logging to wandb.")
+        # Delete process group
+        cleanup_ddp()
+
+    except Exception as e:
+        # In case of an error, put the exception in the result queue
+        result_queue.put((rank, False, 0, str(e)))
+        sys.exit(1)  # Exit with a non-zero code to indicate failure
+
+    # Put return info on queue
+    result_queue.put((rank, complete, pm.epoch, None))
+    sys.exit(0)  # Exit with zero to indicate success
 
 
 def parse_args():
@@ -679,9 +979,11 @@ def train_local():
 
     # Document setup
     document_environment(args.train_dir)
-    document_gpus(args.train_dir)
+    # Cannot document GPU info here because this results in problems with mp.Process
 
-    if args.settings_file is not None:
+    resume = True if args.settings_file is None else False
+
+    if not resume:
         print("Beginning new training run.")
         with open(args.settings_file, "r") as fp:
             train_settings = yaml.safe_load(fp)
@@ -704,20 +1006,41 @@ def train_local():
                     print("wandb not installed, cannot generate run id.")
             yaml.dump(local_settings, f, default_flow_style=False, sort_keys=False)
 
-        pm, wfd = prepare_training_new(
-            train_settings, args.train_dir, local_settings, args.pretraining
-        )
-
     else:
         print("Resuming training run.")
         with open(os.path.join(args.train_dir, "local_settings.yaml"), "r") as f:
             local_settings = yaml.safe_load(f)
-        pm, wfd = prepare_training_resume(
-            args.checkpoint, local_settings, args.train_dir, args.pretraining
-        )
 
-    with threadpool_limits(limits=1, user_api="blas"):
-        complete = train_stages(pm, wfd, args.train_dir, local_settings)
+    # Setup multi-GPU training
+    if (
+        local_settings["device"] == "cuda"
+        and "condor" in local_settings
+        and "num_gpus" in local_settings["condor"]
+        and local_settings["condor"]["num_gpus"] > 1.0
+    ):
+        # Specify the number of processes (typically the number of GPUs available)
+        world_size = local_settings["condor"]["num_gpus"]
+
+        complete, pm_epoch = run_multi_gpu_training(
+            world_size,
+            train_settings,
+            local_settings,
+            args.train_dir,
+            args.checkpoint,
+            resume,
+            args.pretraining,
+        )
+    else:
+        if local_settings["device"] == "cuda":
+            document_gpus(args.train_dir)
+        complete, pm_epoch = run_training(
+            train_settings,
+            local_settings,
+            args.train_dir,
+            args.checkpoint,
+            resume,
+            args.pretraining,
+        )
 
     if complete:
         if args.exit_command:
