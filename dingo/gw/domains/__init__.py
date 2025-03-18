eb9df86a18f7fedcf35fb486f4c454d5eda96a00
Stephen Green <stephenrolandgreen@gmail.com>
2025-03-18 09:07:55 -0700

    Remove adaptive MFD generation.

--- dingo/gw/domains/__init__.py
+++ dingo/gw/domains/__init__.py
@@ -1,8 +1,5 @@
 from .base import Domain
 from .uniform_frequency_domain import UniformFrequencyDomain
 from .time_domain import TimeDomain
-from .multibanded_frequency_domain import (
-    MultibandedFrequencyDomain,
-    get_decimation_bands_adaptive,
-)
+from .multibanded_frequency_domain import MultibandedFrequencyDomain
 from .build_domain import *
