adapt dingo_pipe importance sampling code to work with frequency masking, i.e. setting waveform=0, asds=1 for the masked regions; evidence computation is not corrected yet

dingo/gw/result.py
@@ -18,7 +18,6 @@ from dingo.gw.gwutils import get_extrinsic_prior_dict, get_window_factor
 from dingo.gw.likelihood import StationaryGaussianGWLikelihood
 from dingo.gw.prior import build_prior_with_defaults
 
-
 RANDOM_STATE = 150914
 
 
@@ -71,7 +70,9 @@ class Result(CoreResult):
         # expects detectors to be a list in result.reset_event(). If it is not a list, event_metadata is not the same as
         # the event metadata loaded from the events file (where detectors is a list) and it assumes that a domain update
         # is necessary (which is not implemented for MFD).
-        if self.event_metadata is not None and isinstance(self.event_metadata["detectors"], str):
+        if self.event_metadata is not None and isinstance(
+            self.event_metadata["detectors"], str
+        ):
             self.event_metadata["detectors"] = [self.event_metadata["detectors"]]
 
     @property
@@ -282,6 +283,7 @@ class Result(CoreResult):
         phase_marginalization_kwargs: Optional[dict] = None,
         calibration_marginalization_kwargs: Optional[dict] = None,
         phase_grid: Optional[np.ndarray] = None,
+        frequency_update: Optional[dict] = None,
     ):
         """
         Build the likelihood function based on model metadata. This is called at the
@@ -361,6 +363,7 @@ class Result(CoreResult):
             calibration_marginalization_kwargs=calibration_marginalization_kwargs,
             phase_grid=phase_grid,
             use_base_domain=self.use_base_domain,
+            frequency_update=frequency_update,
         )
 
     def sample_synthetic_phase(

dingo/gw/transforms/waveform_transforms.py
@@ -1,6 +1,8 @@
+from typing import Optional
 import numpy as np
 
 from dingo.gw.domains import MultibandedFrequencyDomain, UniformFrequencyDomain
+from gw.domains import multibanded_frequency_domain
 
 
 class DecimateAll(object):
@@ -173,7 +175,7 @@ class DecimateWaveformsAndASDS(object):
                     for k, v in sample["waveform"].items()
                 }
                 sample["asds"] = {
-                    k: self.multibanded_frequency_domain.decimate(v ** 2) ** 0.5
+                    k: self.multibanded_frequency_domain.decimate(v**2) ** 0.5
                     for k, v in sample["asds"].items()
                 }
 
@@ -181,6 +183,186 @@ class DecimateWaveformsAndASDS(object):
                 raise NotImplementedError()
 
         return sample
+
+
+class MaskDataForFrequencyRangeUpdate(object):
+    """
+    Set waveform to zero and ASD to one according to minimum_frequency, maximum_frequency, and suppress_range.
+    """
+
+    def __init__(
+        self,
+        domain: UniformFrequencyDomain | MultibandedFrequencyDomain,
+        minimum_frequency: Optional[float | dict[str, float]] = None,
+        maximum_frequency: Optional[float | dict[str, float]] = None,
+        suppress_range: Optional[list[float, float] | dict[str, list[float]]] = None,
+        ifos: Optional[list[str]] = None,
+        print_output: bool = False,
+    ):
+        """
+        Parameters
+        ----------
+        minimum_frequency: Optional[float | dict[str, float]]
+            Update of f_min, if float, the same value will be used for all detectors.
+        maximum_frequency: Optional[float | dict[str, float]]
+            Update of f_max, if float, the same value will be used for all detectors.
+        suppress_range: list[float, float] | dict[str, list[float, float]] | None
+            Suppress ranges [f_min, f_max], either for all detectors or for individual detectors. Frequency bins
+            corresponding to f_min and f_max are excluded.
+        ifos: list[str]
+            List of detectors
+        print_output: bool
+            Whether to write print statements to the console.
+        """
+        self.sample_frequencies = domain.sample_frequencies
+        self.frequency_mask = domain.frequency_mask
+        self.minimum_frequency = minimum_frequency
+        self.maximum_frequency = maximum_frequency
+        self.suppress_range = suppress_range
+        # Check that minimum-/maximum frequency is provided for each detector
+        if isinstance(minimum_frequency, dict) and ifos is not None:
+            ifos_min = [i for i in minimum_frequency.keys()]
+            if not set(ifos).issubset(ifos_min):
+                raise ValueError(
+                    f"minimum-frequency={minimum_frequency} doesn't contain information about all "
+                    f"detectors present in event: {ifos}."
+                )
+        if isinstance(maximum_frequency, dict) and ifos is not None:
+            ifos_max = [i for i in maximum_frequency.keys()]
+            if not set(ifos).issubset(ifos_max):
+                raise ValueError(
+                    f"maximum-frequency={maximum_frequency} doesn't contain information about all "
+                    f"detectors present in event: {ifos}."
+                )
+
+        if print_output:
+            print(
+                f"Transform MaskDataForFrequencyRangeUpdate activated:"
+                f"  Settings: \n"
+                f"    - Minimum_frequency update: {self.minimum_frequency}\n"
+                f"    - Maximum_frequency update: {self.maximum_frequency}\n"
+                f"    - Suppress range: {self.suppress_range}\n"
+            )
+
+    def __call__(self, input_sample: dict) -> dict:
+        """
+        Parameters
+        ----------
+        input_sample: Dict
+            Values for keys
+            - 'waveform', shape [batch_size, num_frequency_bins]
+            - 'asds', shape [batch_size, num_frequency_bins]
+
+        Returns
+        ----------
+        sample: Dict
+            input_sample with modified value for key
+            - 'waveform'
+            - 'asds'
+
+        """
+        sample = input_sample.copy()
+        # Get frequency masks for full domain
+        frequency_masks = create_mask_based_on_frequency_update(
+            sample_frequencies=self.sample_frequencies,
+            detectors=[d for d in sample["waveform"].keys()],
+            minimum_frequency=self.minimum_frequency,
+            maximum_frequency=self.maximum_frequency,
+            suppress_range=self.suppress_range,
+        )
+
+        # Update waveforms and ASDs based on masks
+        for d in sample["waveform"].keys():
+            # Combine with frequency mask from domain
+            combined_mask = np.where(self.frequency_mask, ~frequency_masks[d], False)
+            # (1) Zero waveform
+            sample["waveform"][d][..., combined_mask] = 0.0
+            # (2) Set ASD to 1.
+            sample["asds"][d][..., combined_mask] = 1.0
+
+        return sample
+
+
+def create_mask_based_on_frequency_update(
+    sample_frequencies: np.array,
+    detectors: list[str],
+    minimum_frequency: Optional[float | dict[str, float]] = None,
+    maximum_frequency: Optional[float | dict[str, float]] = None,
+    suppress_range: Optional[list[float, float] | dict[str, list[float, float]]] = None,
+) -> dict[str : np.array]:
+    """
+    Parameters
+    ----------
+    sample_frequencies: np.array
+        Frequency values for which we want to create a mask. Assumed to be the same for all detectors.
+    detectors: list[str]
+        List of detector names.
+    minimum_frequency: Optional[float | dict[str, float]]
+        Update of f_min, if float, the same value will be used for all detectors.
+    maximum_frequency: Optional[float | dict[str, float]]
+        Update of f_max, if float, the same value will be used for all detectors.
+    suppress_range: list[float, float] | dict[str, list[float, float]] | None
+        Suppress ranges [f_min, f_max], either for all detectors or for individual detectors. Frequency bins
+        corresponding to f_min and f_max are excluded.
+
+    Returns
+    ----------
+    frequency_masks: dict
+        Dictionary providing a boolean mask for the sample frequencies of each detector based on provided frequency
+        updates. True for values to keep, False for values to mask.
+    """
+    # We only modify the valid frequency values and assume that values corresponding to frequency_mask = False have
+    # already been adjusted
+    frequency_masks = {
+        i: np.ones_like(sample_frequencies, dtype=bool) for i in detectors
+    }
+    for d in detectors:
+        # Update frequency_masks based on minimum_frequency
+        if minimum_frequency is not None:
+            # Same for all detectors
+            if isinstance(minimum_frequency, float):
+                mask_min = sample_frequencies >= minimum_frequency
+            # Different for each detector
+            elif isinstance(minimum_frequency, dict):
+                mask_min = sample_frequencies >= minimum_frequency[d]
+            else:
+                raise ValueError(
+                    f"minimum-frequency has to be dict or float, not {type(minimum_frequency)}. "
+                )
+            frequency_masks[d] = np.logical_and(frequency_masks[d], mask_min)
+
+        # Update frequency_masks based on maximum_frequency
+        if maximum_frequency is not None:
+            # Same for all detectors
+            if isinstance(maximum_frequency, float):
+                mask_max = sample_frequencies <= maximum_frequency
+            # Different for each detector
+            elif isinstance(maximum_frequency, dict):
+                mask_max = sample_frequencies <= maximum_frequency[d]
+            else:
+                raise ValueError(
+                    f"maximum-frequency has to be dict or float, not {type(maximum_frequency)}. "
+                )
+            frequency_masks[d] = np.logical_and(frequency_masks[d], mask_max)
+
+        # Update frequency_masks based on suppress_range
+        if suppress_range is not None:
+            # Same for all detectors
+            if isinstance(suppress_range, list):
+                f_min_lower, f_max_upper = suppress_range
+                mask_lower = sample_frequencies >= f_min_lower
+                mask_upper = sample_frequencies <= f_max_upper
+                mask_interval = ~np.logical_and(mask_lower, mask_upper)
+                frequency_masks[d] = np.logical_and(frequency_masks[d], mask_interval)
+            # Different for each detector
+            elif isinstance(suppress_range, dict) and d in suppress_range:
+                f_min_lower, f_max_upper = suppress_range[d]
+                mask_lower = sample_frequencies >= f_min_lower
+                mask_upper = sample_frequencies <= f_max_upper
+                mask_interval = ~np.logical_and(mask_lower, mask_upper)
+                frequency_masks[d] = np.logical_and(frequency_masks[d], mask_interval)
+
+    return frequency_masks
 
 
 def check_sample_in_domain(sample, domain: UniformFrequencyDomain) -> bool: