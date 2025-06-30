f408bd99d5b9f0832203158f20c49db101779110
Annalena Kofler <annalena.kofler@tuebingen.mpg.de>
2025-06-30 17:45:14 +0200

    Added and corrected skymap code from Max's BNS branch

---
+++ dingo/gw/skymap/dingo_skymap.py
@@ -0,0 +1,158 @@
+"""Generation of ligo skymaps from dingo results."""
+
+from ligo.skymap import kde
+from typing import Optional, Dict, Any
+from bilby.gw.prior import UniformComovingVolume
+from bilby.core.prior.analytical import PowerLaw
+import numpy as np
+
+from dingo.gw.result import Result
+
+
+def generate_skymap_from_dingo_result(
+    dingo_result: Result,
+    num_samples: int = 5_000,
+    num_trials: int = 1,
+    num_jobs: int = 1,
+    prior_distance_power: Optional[float] = 2,
+    cosmology: bool = False,
+    weight_clipping_kwargs: Optional[Dict[str, Any]] = None,
+    return_aux: bool = False,
+    allow_duplicates: bool = True,
+):
+    """Generate a skymap based on the estimated sky position of the dingo result.
+
+    Parameters
+    ----------
+    dingo_result: Result
+        dingo result file
+    num_samples: int
+        number of samples for skymap kde
+    num_trials: int
+        number of trials for skymap kde
+    num_jobs: int
+        number of parallel job for skymap kde
+    prior_distance_power: int, optional
+        The power of distance that appears in the prior
+        default: 2, uniform in volume
+        if None, use prior from dingo result (i.e., no reweighting)
+    cosmology: bool, optional
+        Set to enable a uniform in comoving volume prior (default: false).
+    weight_clipping_kwargs: dict
+        if set, clip the weights before rejection sampling based on these kwargs
+    return_aux: bool
+        if True, return dict with aux information
+    allow_duplicates: bool
+        if False, the KDE samples are sampled without replacement
+
+    Returns
+    -------
+    skymap: skymap for estimated dingo position
+    aux: dict with aux information
+    """
+    samples = dingo_result.samples
+
+    # extract weights, also veto invalid [ra, dec] values
+    if "weights" in samples:
+        weights = np.array(samples["weights"])
+    else:
+        weights = np.ones(len(samples))
+    weights *= np.array(samples["ra"]) >= 0
+    weights *= np.array(samples["ra"]) <= 2 * np.pi
+    weights *= np.array(samples["dec"]) >= -np.pi / 2
+    weights *= np.array(samples["dec"]) <= np.pi / 2
+
+    # apply distance reweighting
+    distance = np.array(samples["luminosity_distance"])
+    prior_result = dingo_result.prior["luminosity_distance"]
+    if cosmology:
+        if prior_distance_power is not None:
+            raise ValueError(
+                "Only one of prior_distance_power and cosmology can be used."
+            )
+        prior_updated = UniformComovingVolume(
+            prior_result.minimum, prior_result.maximum, name="luminosity_distance"
+        )
+        weights = weights * np.exp(
+            prior_updated.ln_prob(distance) - prior_result.ln_prob(distance)
+        )
+        weights[np.where(np.isinf(prior_result.ln_prob(distance)))[0]] = 0
+        weights /= weights.mean()
+    elif prior_distance_power is not None:
+        prior_updated = PowerLaw(
+            prior_distance_power, prior_result.minimum, prior_result.maximum
+        )
+        weights = weights * np.exp(
+            prior_updated.ln_prob(distance) - prior_result.ln_prob(distance)
+        )
+        weights[np.where(np.isinf(prior_result.ln_prob(distance)))[0]] = 0
+        weights /= weights.mean()
+
+    if weight_clipping_kwargs is not None:
+        if not allow_duplicates:
+            raise ValueError(
+                "Weight clipping should not be set when dropping duplicates."
+            )
+        weights = clip_weights(weights, **weight_clipping_kwargs)
+
+    samples = samples.sample(num_samples, weights=weights, replace=allow_duplicates)
+    ra_dec_dL = np.array(samples[["ra", "dec", "luminosity_distance"]])
+    skypost = kde.Clustered2DSkyKDE(ra_dec_dL, trials=num_trials, jobs=num_jobs)
+    skymap = skypost.as_healpix()
+
+    if not return_aux:
+        return skymap
+    else:
+        aux = {
+            "n_eff": int(np.sum(weights) ** 2 / np.sum(weights**2)),
+            "n_rejection": int(np.sum(weights / np.max(weights))),
+        }
+        return skymap, aux
+
+
+def clip_weights(weights, num_clip, mode="mean", print_stats=False):
+    """Clip the num_clip highest weights and return the result.
+
+    See e.g. https://ieeexplore.ieee.org/document/8450722.
+    Clipping is leads to asymptotically correct results if num_clip <= sqrt(num_samples).
+
+    Parameters
+    ----------
+    weights: array with weights
+    num_clip: number of samples to clip
+    mode: whether to set the clipped weight to mean or min weight of the clipped samples
+    print_stats: if True, print the ess and ns before and after clipping
+
+    Returns
+    -------
+    weights: updated (clipped) weights
+    """
+    weights = np.copy(weights)
+    ess_before = np.sum(weights) ** 2 / np.sum(weights**2)
+    ns_before = np.sum(weights / np.max(weights))
+
+    # get indices of clipped samples
+    # clipped_indices = np.argsort(-weights)[:num_clip]
+    clipped_indices = np.argpartition(-weights, num_clip)[:num_clip]  # more efficient
+
+    # get new weight for clipped samples
+    if mode == "mean":
+        weight_new = np.mean(weights[clipped_indices])
+    elif mode == "min":
+        weight_new = np.min(weights[clipped_indices])
+    else:
+        raise ValueError(f"Mode should be mean or min, got {mode}.")
+
+    # set new weight
+    weights[clipped_indices] = weight_new
+    ess_after = np.sum(weights) ** 2 / np.sum(weights**2)
+    ns_after = np.sum(weights / np.max(weights))
+
+    if print_stats:
+        print(f"Statistics before / after clipping (N = {len(weights)}):")
+        print(f"ESS:\t{ess_before:.0f} / {ess_after:.0f}")
+        print(f"NS: \t{ns_before:.0f} / {ns_after:.0f}")
+
+    weights = weights / np.mean(weights)
+
+    return weights
---
+++ dingo/gw/skymap/utils.py
@@ -0,0 +1,156 @@
+"""Util functions for skymap conversions and analysis"""
+
+import numpy as np
+import healpy as hp
+import pandas as pd
+
+from ligo.skymap.postprocess import crossmatch
+from ligo.skymap.postprocess.util import find_greedy_credible_levels
+from ligo.skymap.moc import rasterize
+
+
+def skymap_from_uniq_to_nested(skymap, normalize=True, order=-1):
+    """Convert skymap from unique identifier convention to nested convention.
+
+    See https://healpix.sourceforge.io/html/intro_Geometric_Algebraic_Propert.htm.
+    HEALPix pixels are defined by three quantities:
+
+        nside: resolution, = 2 ** order
+        p: pixel index
+
+    For a fixed nside, the pixel values can simply be provided as a list. The length of
+    the list determines nside [nside = hp.npix2nside(len(pixels))] and each position in
+    the list corresponds to a specific position. There are two conventions for the
+    positions, Ring and Nested.
+
+    In addition to the Ring and Nested conventions, there is the Unique Identifier
+    scheme, in which the resolution nside and pixel position p are mergerd into a
+    single unique identifier u.
+
+        u = p + 4 * nside ** 2
+
+    With this convention, the pixel position is no longer defined by the position in
+    the list, but rather by the unique id u. This is useful when specifying the pixels
+    at multiple different resolutions (or for sparse maps).
+
+    ligo.skymap often uses the unique identifier scheme and this function converts from
+    this convention to nested convention.
+
+    Parameters
+    ----------
+    skymap: input skymap in unique identifier convention
+    normalize: if True, normalize skymap to sum 1.
+    order: order for rasterization, if -1 use max resolution of skymap
+
+    Returns
+    -------
+    skymap: output skymap as numpy array in nested convention
+    """
+    if "UNIQ" not in skymap.columns:
+        raise ValueError("Skymap convention must be unique identifier scheme.")
+
+    # step 1: rasterize to remove multi-order pixels
+    # pixel labeling: UNIQ ids => Ring
+    skymap = pd.DataFrame(rasterize(skymap, order=order))
+    skymap = np.array(skymap["PROBDENSITY"])
+
+    # step 2: convert from ring to nested pixel labels
+    nside = hp.npix2nside(len(skymap))
+    reordering = hp.ring2nest(nside, np.arange(len(skymap)))
+    skymap = skymap[reordering]
+
+    # step 3: optionally normalize
+    if normalize:
+        skymap /= skymap.sum()
+
+    return skymap
+
+
+def credible_areas(skymap, credible_levels):
+    """Compute areas (in deg^2) at the specified credible levels."""
+    if np.min(credible_levels) < 0 or np.max(credible_levels) > 1:
+        raise ValueError(f"Contours must be in range [0, 1], got {credible_levels}")
+    return crossmatch(skymap, contours=credible_levels).contour_areas
+
+
+def coverage(
+    skymap_proposal, skymap_reference, credible_levels, check_consistency=False
+):
+    """Compute skymap_proposal's coverage fraction of skymap_reference at credible_levels.
+
+    For each credible level p, this computes the fraction f of skymap_reference that is
+    covered by the p-credible-area of skymap_proposal.
+
+    For a perfect overlap between proposal and reference skymap, we expect f = p.
+
+    Parameters
+    ----------
+    skymap_proposal: proposal skymap
+    skymap_reference: reference skymap
+    credible_levels: credible levels at which to evaluate coverage
+    check_consistency: If True, check that the coverages of the proposal with itself are
+        consistent with the credible_levels input. This sanity check may fail due to
+        quantization errors when the sky area is too tight and only consists of a few
+        pixels.
+
+    Returns
+    -------
+    coverages: skymap_proposal's coverage fraction of skymap_reference
+    """
+    if np.min(credible_levels) < 0 or np.max(credible_levels) > 1:
+        raise ValueError(f"Contours must be in range [0, 1], got {credible_levels}")
+
+    # convert to nested convention
+    if "UNIQ" in skymap_proposal.columns:
+        skymap_proposal = skymap_from_uniq_to_nested(skymap_proposal, order=10)
+    if "UNIQ" in skymap_reference.columns:
+        skymap_reference = skymap_from_uniq_to_nested(skymap_reference, order=10)
+
+    # find credible levels
+    credible_levels_proposal = find_greedy_credible_levels(skymap_proposal)
+    credible_levels_reference = find_greedy_credible_levels(skymap_reference)
+
+    # compute coverage of credible areas of skymap_reference at specified credible_levels
+    # by skymap_proposal
+    coverages = []
+    coverages_proposal = []  # sanity check
+    for contour in credible_levels:
+        coverages.append(
+            np.sum(skymap_reference * (credible_levels_proposal <= contour))
+        )
+        coverages_proposal.append(
+            np.sum(skymap_reference * (credible_levels_reference <= contour))
+        )
+
+    # check that proposal coverages are close to input credible_levels
+    if check_consistency:
+        deviation = np.array(coverages_proposal) - np.array(credible_levels)
+        assert np.max(np.abs(deviation)) < 1e-3, (coverages, coverages_proposal)
+
+    return coverages
+
+
+def credible_levels_at_position(skymap, ra, dec, order=-1):
+    """Compute credible level at which position [ra, dec] is covered by skymap.
+
+    Parameters
+    ----------
+    skymap: skymap with estimated position
+    ra: ra coordinate in rad, array or float
+    dec: dec coordinate in rad, array or float
+    order: order for rasterization, if -1 use max resolution of skymap
+
+    Returns
+    -------
+    credible_level: credible level at which [ra, dec] is covered by skymap, array or float
+    """
+    # convert to nested convention
+    if "UNIQ" in skymap.columns:
+        skymap = skymap_from_uniq_to_nested(skymap, order=order)
+    # compute pixel id corresponding to [ra, dec]
+    nside = hp.npix2nside(len(skymap))
+    ipix = hp.ang2pix(nside, 0.5 * np.pi - dec, ra)  # ra, dec in rad!
+    # find credible levels
+    credible_levels = find_greedy_credible_levels(skymap)
+    # return credible level at pixel location of [ra, dec]
+    return credible_levels[ipix]
