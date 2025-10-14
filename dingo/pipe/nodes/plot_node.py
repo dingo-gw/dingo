704f7e96972b16528180b7d4350055f25180e43c
nihargupte-ph <ngupte@umd.edu>
2025-10-14 02:56:08 -0700

    allowed for file transfer for merge, pesummary and plotting nodes

--- dingo/pipe/nodes/plot_node.py
+++ dingo/pipe/nodes/plot_node.py
@@ -1,3 +1,5 @@
+import os
+
 from bilby_pipe.job_creation.nodes import PlotNode as BilbyPlotNode
 
 from dingo.pipe.utils import _strip_unwanted_submission_keys
@@ -8,17 +10,44 @@ class PlotNode(BilbyPlotNode):
     run_node_on_osg = False
     
     def __init__(self, inputs, merged_node, dag):
+        # Call Node.__init__ directly, skipping BilbyPlotNode.__init__ to customize
         super(BilbyPlotNode, self).__init__(inputs)
         self.dag = dag
         self.job_name = merged_node.job_name.replace("_merge", "") + "_plot"
         self.label = merged_node.job_name.replace("_merge", "") + "_plot"
         self.request_cpus = 1
+
+        # Add file transfer logic (copied from BilbyPlotNode)
+        if self.inputs.transfer_files or self.inputs.osg:
+            input_files_to_transfer = [
+                self._relative_topdir(merged_node.result_file, self.inputs.initialdir),
+            ] + inputs.additional_transfer_paths
+            if self.transfer_container:
+                input_files_to_transfer.append(self.inputs.container)
+
+            input_files_to_transfer, need_scitokens = self.job_needs_authentication(
+                input_files_to_transfer
+            )
+            if need_scitokens:
+                self.extra_lines.extend(self.scitoken_lines)
+
+            self.extra_lines.extend(
+                self._condor_file_transfer_lines(
+                    input_files_to_transfer,
+                    [self._relative_topdir(self.inputs.outdir, self.inputs.initialdir)],
+                )
+            )
+
         self.setup_arguments(
             add_ini=False, add_unknown_args=False, add_command_line_args=False
         )
         self.arguments.add("label", self.label)
-        self.arguments.add("result", merged_node.result_file)
-        self.arguments.add("outdir", self.inputs.result_directory)
+        if self.inputs.transfer_files or self.inputs.osg:
+            self.arguments.add("result", os.path.relpath(merged_node.result_file))
+            self.arguments.add("outdir", os.path.relpath(self.inputs.result_directory))
+        else:
+            self.arguments.add("result", merged_node.result_file)
+            self.arguments.add("outdir", self.inputs.result_directory)
         for plot_type in ["corner", "weights", "log_probs"]:
             if getattr(inputs, f"plot_{plot_type}", False):
                 self.arguments.add_flag(plot_type)
@@ -62,3 +91,7 @@ class PlotPPNode(BilbyPlotNode):
     @property
     def executable(self):
         return self._get_executable_path("dingo_pipe_pp_test")
+
+    @property
+    def request_memory(self):
+        return "16 GB"
