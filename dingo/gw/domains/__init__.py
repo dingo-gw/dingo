16297cdb79b22a45ca0f8ceb1994e2304435e70f
Annalena Kofler <annalena.kofler@tuebingen.mpg.de>
2026-03-02 13:48:12 +0100

    Fix bug in import order crashing the CI test

--- dingo/gw/domains/__init__.py
+++ dingo/gw/domains/__init__.py
@@ -1,5 +1,6 @@
 from .base import Domain
-from .build_domain import *
 from .multibanded_frequency_domain import MultibandedFrequencyDomain
 from .time_domain import TimeDomain
 from .uniform_frequency_domain import UniformFrequencyDomain
+
+from .build_domain import *  # isort: skip
